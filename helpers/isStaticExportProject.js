da89b54d458b3ac7c78ca7b7f43c3f87019cdc68
Lindsay Levine <lindslev.ll@gmail.com>
2020-10-26 23:34:41 -0400

    adjust plugin to require functions dir to be set as out_functions and catch other edge cases

---
+++ helpers/isStaticExportProject.js
@@ -0,0 +1,14 @@
+// Takes 1. Netlify config's build details and
+// 2. the project's package.json scripts to determine if
+// the Next.js app uses static HTML export
+
+const isStaticExportProject = ({ build, scripts }) => {
+  const NEXT_EXPORT_COMMAND = 'next export';
+  const isSetInNetlifyConfig = build && build.command && build.command.includes(NEXT_EXPORT_COMMAND);
+  const isSetInNpmScript = Object.keys(scripts).find((script) => {
+    return scripts[script].includes(NEXT_EXPORT_COMMAND);
+  });
+  return isSetInNetlifyConfig || isSetInNpmScript;
+};
+
+module.exports = isStaticExportProject;
--- index.js
+++ index.js
@@ -8,16 +8,12 @@ const { PHASE_PRODUCTION_BUILD } = require('next/constants');
 const { default: loadConfig } = require('next/dist/next-server/server/config');
 const makeDir = require('make-dir');
 const cpx = require('cpx');
+const isStaticExportProject = require('./helpers/isStaticExportProject');
 
 const _isNextProject = async () => {
   const frameworks = await listFrameworks();
   return !!frameworks.find(({ name }) => name === 'next');
 };
-
-// TO-DO:
-// - add try catches and error handling
-// - edge cases:
-//    - making sure functions_dir doesnt have a custom function named something that NoN could generate
 
 // * Helpful Plugin Context *
 // - Between the prebuild and build steps, the project's build command is run
@@ -30,18 +26,40 @@ module.exports = {
     const { failBuild } = utils.build;
 
     if (isNextProject) {
-      // TO-DO: read scripts from package.json and test this there
-      const isStaticExport = build && build.command && build.command.includes('next export');
-      if (isStaticExport) {
+      // TO-DO: Post alpha, try to remove this workaround for missing deps in
+      // the next-on-netlify function template
+      await utils.run.command('npm install next-on-netlify@latest');
+
+      // Require the project's package.json for access to its scripts
+      // and dependencies in order to check existing project configuration
+      let packageJson;
+      if (existsSync(path.resolve(constants.PUBLISH_DIR, 'package.json'))) {
+        packageJson = require(path.resolve(constants.PUBLISH_DIR, 'package.json'));
+      } else if (existsSync(path.resolve(constants.PUBLISH_DIR, '..', 'package.json'))) {
+        packageJson = require(path.resolve(constants.PUBLISH_DIR, '..', 'package.json'));
+      } else {
+        failBuild(`Cannot locate your package.json file. Please make sure your package.json is
+          at the root of your project or in your publish directory.`
+        );
+      }
+
+      const { scripts, dependencies } = packageJson;
+
+      if (isStaticExportProject({ build, scripts })) {
         failBuild(`** Static HTML export next.js projects do not require this plugin **`);
       }
 
-      // TO-DO: read dependencies from package.json
-      // fail build if next-on-netlify is already installed or in a script?
+      // TO-DO: check scripts to make sure the app isn't manually running NoN
+      // For now, we'll make it clear in the README
+      // const isAlreadyUsingNextOnNetlify = Object.keys(dependencies).find((dep) => dep === 'next-on-netlify');
+      // if (isAlreadyUsingNextOnNetlify) {
+      //   failBuild(`This plugin cannot support apps that manually use next-on-netlify. Uninstall next-on-netlify as a dependency to resolve.`);
+      // }
 
-      const hasFunctionsDirectorySet = build && netlifyConfig.functions;
-      if (!hasFunctionsDirectorySet) {
-        failBuild
+      const isFunctionsDirectoryCorrect = build && build.functions && build.functions === path.resolve('out_functions');
+      if (!isFunctionsDirectoryCorrect) {
+        // to do rephrase
+        failBuild(`You must designate a functions directory named "out_functions" in your netlify.toml or in your app's build settings on Netlify. See docs for more info: https://docs.netlify.com/functions/configure-and-deploy/#configure-the-functions-folder`);
       }
 
       if (existsSync('next.config.js')) {
@@ -75,18 +93,20 @@ module.exports = {
 
       // Next-on-netlify puts its files into out_functions and out_publish
       // Copy files from next-on-netlify's output to the right functions/publish dirs
-      const { FUNCTIONS_DIST, PUBLISH_DIR } = constants;
-      await makeDir(FUNCTIONS_DIST);
-      await makeDir(PUBLISH_DIR);
-      cpx.copySync('out_functions/**/*', FUNCTIONS_DIST);
+
+      // TO-DO: use FUNCTIONS_DIST when internal bug is fixed
+      const { PUBLISH_DIR } = constants;
+      // if (!existsSync(FUNCTIONS_DIST)) {
+      //   await makeDir(FUNCTIONS_DIST);
+      // }
+      if (!existsSync(PUBLISH_DIR)) {
+        await makeDir(PUBLISH_DIR);
+      }
+
+      // TO-DO: make sure FUNCTIONS_DIST doesnt have a custom function name conflict
+      // with function names that next-on-netlify can generate
+      // cpx.copySync('out_functions/**/*', FUNCTIONS_SRC);
       cpx.copySync('out_publish/**/*', PUBLISH_DIR);
     }
-  },
-  async onPostBuild({ constants }) {
-    const { FUNCTIONS_DIST, PUBLISH_DIR } = constants;
-    await makeDir(FUNCTIONS_DIST);
-    await makeDir(PUBLISH_DIR);
-    cpx.copySync('out_functions/**/*', FUNCTIONS_DIST);
-    cpx.copySync('out_publish/**/*', PUBLISH_DIR);
   }
 }
