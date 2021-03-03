813475415bbb070260a225f69b2ecb5ce0623100
Lindsay Levine <lindslev.ll@gmail.com>
2021-03-03 18:39:20 -0500

    fix: 10.0.8 changes loadConfig to be async

--- helpers/hasCorrectNextConfig.js
+++ helpers/hasCorrectNextConfig.js
@@ -1,7 +1,7 @@
 const path = require('path')
 
 // Checks if site has the correct next.cofig.js
-const hasCorrectNextConfig = ({ nextConfigPath, failBuild }) => {
+const hasCorrectNextConfig = async ({ nextConfigPath, failBuild }) => {
   // In the plugin's case, no config is valid because we'll make it ourselves
   if (nextConfigPath === undefined) return true
 
@@ -14,7 +14,7 @@ const hasCorrectNextConfig = ({ nextConfigPath, failBuild }) => {
   const acceptableTargets = ['serverless', 'experimental-serverless-trace']
   let nextConfig
   try {
-    nextConfig = loadConfig(PHASE_PRODUCTION_BUILD, path.resolve('.'))
+    nextConfig = await loadConfig(PHASE_PRODUCTION_BUILD, path.resolve('.'))
   } catch (error) {
     return failBuild('Error loading your next.config.js.', { error })
   }
