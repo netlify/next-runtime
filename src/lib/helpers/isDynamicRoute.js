61674ba83dcdb120e26e01ffbb4d676f4995b3cd
Lindsay Levine <lindslev.ll@gmail.com>
2021-05-03 16:35:38 -0400

    fix: support isDynamicRoute in sls-next >=1.8.0

--- src/lib/helpers/isDynamicRoute.js
+++ src/lib/helpers/isDynamicRoute.js
@@ -1,4 +1,8 @@
 // Return true if the route uses dynamic routing (e.g., [id] or [...slug])
-const { default: isDynamicRoute } = require('@sls-next/lambda-at-edge/dist/lib/isDynamicRoute')
+// @sls-next <= 1.7.0 uses default, >= 1.8.0 no longer does
+const {
+  default: oldIsDynamicRoute,
+  isDynamicRoute = oldIsDynamicRoute,
+} = require('@sls-next/lambda-at-edge/dist/lib/isDynamicRoute')
 
 module.exports = isDynamicRoute
