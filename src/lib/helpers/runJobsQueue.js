b27f30d3749e9bf36f99fb330c6fa488b58f7a69
Matt Kane <matt@gatsbyjs.com>
2021-05-21 09:21:18 +0100

    fix: changes from review

--- src/lib/helpers/runJobsQueue.js
+++ src/lib/helpers/runJobsQueue.js
@@ -4,10 +4,13 @@ const fastq = require('fastq')
 
 const { processPage } = require('../pages/worker')
 
+// TODO: benchmark a large site to find a good value for this
+const PAGE_CONCURRENCY = 4
+
 const runJobsQueue = (jobs) => {
   console.log(`Building ${jobs.length} pages`)
 
-  const queue = fastq.promise(processPage, 4)
+  const queue = fastq.promise(processPage, PAGE_CONCURRENCY)
 
   return Promise.all(jobs.map((job) => queue.push(job)))
 }
