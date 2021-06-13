c3818b583b156a1646cc2d2c4e4659109347068b
Tom Milewski <me@tm.codes>
2021-06-13 13:21:26 -0400

    fix: clearer error messaging
    To help with debugging across environments

--- src/lib/steps/copyNextAssets.js
+++ src/lib/steps/copyNextAssets.js
@@ -7,12 +7,16 @@ const { logTitle } = require('../helpers/logger')
 
 // Copy the NextJS' static assets from NextJS distDir to Netlify publish folder.
 // These need to be available for NextJS to work.
+
+// Note: If you're using a custom build setup, such as a monorepo, 
+//       you may need to explicitly set distDir in your next.config.js.
+
 const copyNextAssets = async (publishPath) => {
   const nextDistDir = await getNextDistDir()
   const staticAssetsPath = join(nextDistDir, 'static')
   if (!existsSync(staticAssetsPath)) {
     throw new Error(
-      'No static assets found in .next dist (aka no /.next/static). Please check your project configuration. Your next.config.js must be one of `serverless` or `experimental-serverless-trace`. Your build command should include `next build`.',
+      `No static assets found in your distDir (missing /static in ${nextDistDir}). Please check your project configuration. Your next.config.js must be one of \`serverless\` or \`experimental-serverless-trace\`. Your build command should include \`next build\`.`,
     )
   }
   logTitle('💼 Copying static NextJS assets to', publishPath)
