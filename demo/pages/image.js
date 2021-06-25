b6baef944a24e40e262330465c6832e9a7f49705
Matt Kane <matt@gatsbyjs.com>
2021-06-25 16:21:24 +0100

    chore: add image credit

--- demo/pages/image.js
+++ demo/pages/image.js
@@ -3,7 +3,14 @@ import img from './unsplash.jpg'
 
 const Images = () => (
   <div>
-    <Image src={img} width={1200} height={800} alt="puppy" />
+    <Image src={img} width={1200} height={800} alt="shiba inu dog looks through a window" />
+    <p>
+      <a href="https://unsplash.com/photos/DVCyb0lssMk">Photo</a> by{' '}
+      <a href="https://unsplash.com/@veraduez?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">
+        Vera Duez
+      </a>{' '}
+      on <a href="https://unsplash.com/?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText">Unsplash</a>
+    </p>
   </div>
 )
 
