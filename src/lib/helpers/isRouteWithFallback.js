f99bf6695c5f3acf7c6608ee3f9183d062b89fbf
Lindsay Levine <lindslev.ll@gmail.com>
2021-08-01 06:12:18 -0400

    fix: revalidate pages with fallback werent getting new preview redirects

--- src/lib/helpers/isRouteWithFallback.js
+++ src/lib/helpers/isRouteWithFallback.js
@@ -1,6 +1,8 @@
 const getPrerenderManifest = require('./getPrerenderManifest')
 
 const isRouteWithFallback = async (route) => {
+  if (!route) return false
+
   const { dynamicRoutes } = await getPrerenderManifest()
 
   // Fallback "blocking" routes will have fallback: null in manifest
