d3d437e8602fba390f0ac37f6df5c2788f23fbbd
Lindsay Levine <lindslev.ll@gmail.com>
2021-08-26 12:18:45 -0400

    fix: inline isDynamicRoute & uninstall sls-next

--- src/lib/helpers/isDynamicRoute.js
+++ src/lib/helpers/isDynamicRoute.js
@@ -1,8 +1,6 @@
 // Return true if the route uses dynamic routing (e.g., [id] or [...slug])
-// @sls-next <= 1.7.0 uses default, >= 1.8.0 no longer does
-const {
-  default: oldIsDynamicRoute,
-  isDynamicRoute = oldIsDynamicRoute,
-} = require('@sls-next/lambda-at-edge/dist/lib/isDynamicRoute')
+// (taken from @sls-next/lambda-at-edge)
+
+const isDynamicRoute = (route) => /\/\[[^/]+?](?=\/|$)/.test(route)
 
 module.exports = isDynamicRoute
