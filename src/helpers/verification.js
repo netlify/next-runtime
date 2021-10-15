1a6a5f422bd0d22fce2a2499add3b33dc04b4468
Matt Kane <m@mk.gg>
2021-10-15 15:35:50 +0100

    chore: update min build version
    Release-As: 4.0.0-beta.1

--- src/helpers/verification.js
+++ src/helpers/verification.js
@@ -13,14 +13,14 @@ exports.verifyBuildTarget = (target) => {
   }
 }
 
-// This is when the esbuild dynamic import support was added
-const REQUIRED_BUILD_VERSION = '>=15.11.5'
+// This is when nft support was added
+const REQUIRED_BUILD_VERSION = '>=18.16.0'
 
 exports.verifyNetlifyBuildVersion = ({ IS_LOCAL, NETLIFY_BUILD_VERSION, failBuild }) => {
   // We check for build version because that's what's available to us, but prompt about the cli because that's what they can upgrade
   if (IS_LOCAL && !satisfies(NETLIFY_BUILD_VERSION, REQUIRED_BUILD_VERSION, { includePrerelease: true })) {
     return failBuild(outdent`
-      This version of the Essential Next.js plugin requires netlify-cli@4.4.2 or higher. Please upgrade and try again.
+      This version of the Essential Next.js plugin requires netlify-cli@6.12.4 or higher. Please upgrade and try again.
       You can do this by running: "npm install -g netlify-cli@latest" or "yarn global add netlify-cli@latest"
     `)
   }
