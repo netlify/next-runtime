fe35a5df79b60a4b579d96843dcdaf103c338e24
Matt Kane <m@mk.gg>
2021-11-05 12:56:41 +0000

    fix: use glob to move files

--- src/helpers/files.js
+++ src/helpers/files.js
@@ -1,45 +1,43 @@
 //  @ts-check
+const { cpus } = require('os')
+
 const { existsSync, readJson, move, cpSync, copy, writeJson } = require('fs-extra')
 const pLimit = require('p-limit')
 const { join } = require('pathe')
+const glob = require('tiny-glob')
 
-const TEST_ROUTE = /\/\[[^/]+?](?=\/|$)/
+const TEST_ROUTE = /(|\/)\[[^/]+?](\/|\.html|$)/
 
 const isDynamicRoute = (route) => TEST_ROUTE.test(route)
 
 exports.moveStaticPages = async ({ netlifyConfig, target, i18n, failBuild }) => {
-  const root = join(netlifyConfig.build.publish, target === 'server' ? 'server' : 'serverless')
-  const pagesManifestPath = join(root, 'pages-manifest.json')
-  if (!existsSync(pagesManifestPath)) {
-    failBuild(`Could not find pages manifest at ${pagesManifestPath}`)
-  }
+  console.log('Moving static page files to serve from CDN...')
+  const root = join(netlifyConfig.build.publish, target === 'server' ? 'server' : 'serverless', 'pages')
+
   const files = []
 
   const moveFile = async (file) => {
     const source = join(root, file)
-    // Trim the initial "pages"
-    const filePath = file.slice(6)
-    files.push(filePath)
-    const dest = join(netlifyConfig.build.publish, filePath)
+    files.push(file)
+    const dest = join(netlifyConfig.build.publish, source)
     await move(source, dest)
   }
+  // Move all static files, except error documents and nft manifests
+  const pages = await glob('**/!(500|404|*.nft).{html,json}', {
+    cwd: root,
+    dot: true,
+  })
 
-  const pagesManifest = await readJson(pagesManifestPath)
-  // Arbitrary limit of 10 concurrent file moves
-  const limit = pLimit(10)
-  const promises = Object.entries(pagesManifest).map(async ([route, filePath]) => {
-    if (
-      isDynamicRoute(route) ||
-      !(filePath.endsWith('.html') || filePath.endsWith('.json')) ||
-      filePath.endsWith('/404.html') ||
-      filePath.endsWith('/500.html')
-    ) {
+  // Limit concurrent file moves to number of cpus or 2 if there is only 1
+  const limit = pLimit(Math.max(2, cpus().length))
+  const promises = pages.map(async (filePath) => {
+    if (isDynamicRoute(filePath)) {
       return
     }
     return limit(moveFile, filePath)
   })
   await Promise.all(promises)
-  console.log(`Moved ${files.length} page files`)
+  console.log(`Moved ${files.length} files`)
 
   // Write the manifest for use in the serverless functions
   await writeJson(join(netlifyConfig.build.publish, 'static-manifest.json'), files)
