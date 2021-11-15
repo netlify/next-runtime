fd7130f4a15f51e0785cdd9515faeb10cffb67a5
Matt Kane <matt.kane@netlify.com>
2021-11-15 16:03:09 +0000

    fix: use forced catchall for preview mode (#793)
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- demo/pages/previewTest.js
+++ demo/pages/previewTest.js
@@ -1,21 +1,12 @@
 import Link from 'next/link'
 
-const StaticTest = ({ number }) => {
+const StaticTest = ({ isPreview }) => {
   return (
     <div>
-      <p>
-        This page uses getStaticProps() and is SSRed when in preview mode.
-        <br />
-        <br />
-        By default, it shows the TV show by ID (as static HTML).
-        <br />
-        But when in preview mode, it shows person by ID instead (SSRed).
-      </p>
 
-      <hr />
 
-      <h1>Number: {number}</h1>
-
+      <h1>Is preview? {isPreview ? "Yes!" : "No"}</h1>
+      <p><a href={isPreview ? "/api/exitPreview" : "/api/enterPreview"}>{isPreview ? "Exit Preview" : "Enter Preview"}</a></p>
       <Link href="/">
         <a>Go back home</a>
       </Link>
@@ -24,20 +15,11 @@ const StaticTest = ({ number }) => {
 }
 
 export const getStaticProps = async ({ preview }) => {
-  let number
 
-  // In preview mode, use odd number
-  if (preview) {
-    number = 3
-  }
-  // In normal mode, use even number
-  else {
-    number = 4
-  }
 
   return {
     props: {
-      number,
+      isPreview: Boolean(preview),
     },
   }
 }
