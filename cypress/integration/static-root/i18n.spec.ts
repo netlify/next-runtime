14fbaec81d2766cf488fef02c121405c164741e3
Matt Kane <m@mk.gg>
2021-12-01 16:27:18 +0000

    chore: fix tests

--- cypress/integration/static-root/i18n.spec.ts
+++ cypress/integration/static-root/i18n.spec.ts
@@ -12,14 +12,22 @@ describe('Localization', () => {
     cy.setCookie('NEXT_LOCALE', 'fr')
     cy.visit('/')
 
-    cy.findByText('The current locale is fr')
     cy.url().should('eq', `${Cypress.config().baseUrl}/fr/`)
+    cy.findByText('The current locale is fr')
+  })
+
+  it('should use the nf_lang cookie to determine the default locale', () => {
+    cy.setCookie('nf_lang', 'fr')
+    cy.visit('/')
+
+    cy.url().should('eq', `${Cypress.config().baseUrl}/fr/`)
+    cy.findByText('The current locale is fr')
   })
 
   it('should use Accept-Language to choose a locale', () => {
     cy.visit('/', {
       headers: {
-        'Accept-Language': 'fr;q=0.9',
+        'Accept-Language': 'fr-FR,fr;q=0.5',
       },
     })
     cy.url().should('eq', `${Cypress.config().baseUrl}/fr/`)
@@ -31,11 +39,10 @@ describe('Localization', () => {
     cy.visit({
       url: '/',
       headers: {
-        'Accept-Language': 'fr;q=0.9',
+        'Accept-Language': 'fr-FR,fr;q=0.5',
       },
     })
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
-
     cy.findByText('The current locale is en')
   })
 })
