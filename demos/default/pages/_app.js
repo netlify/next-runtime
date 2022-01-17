bf0b9bdab7762b267240882dabde4431851610d0
Tiffany Le-Nguyen <tiffany@netlify.com>
2022-01-17 17:06:24 +0300

    chore: add prose css

--- demos/default/pages/_app.js
+++ demos/default/pages/_app.js
@@ -1,7 +1,11 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className='prose ml-14 mt-10 mb-20'>
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
