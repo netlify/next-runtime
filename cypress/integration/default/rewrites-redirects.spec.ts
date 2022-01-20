913c45a25470bca9e57521e5da05aa1c931ba537
Tiffany Le-Nguyen <tiffany@netlify.com>
2022-01-20 20:07:42 +0300

    chore: cypress action move (#1138)

--- cypress/integration/default/rewrites-redirects.spec.ts
+++ cypress/integration/default/rewrites-redirects.spec.ts
@@ -2,7 +2,7 @@ describe('Rewrites and Redirects', () => {
   it('rewrites: points /old to /', () => {
     // preview mode is off by default
     cy.visit('/old')
-    cy.findByText('Next Demo!')
+    cy.findByText('NextJS on Netlify (imported Header component)')
     cy.url().should('eq', `${Cypress.config().baseUrl}/old/`)
 
     // ensure headers are still set
