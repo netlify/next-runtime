e8067bf13ec94fac80ca6ce495a32249dcd5130c
Matt Kane <matt.kane@netlify.com>
2022-02-03 13:34:21 +0000

    fix: correctly disable ISR disk flushing (#1190)
    * chore: add blocking ISR demo
    
    * fix: ensure all files are patched correctly

---
+++ demos/default/pages/getStaticProps/withRevalidate/withFallbackBlocking/[id].js
@@ -0,0 +1,45 @@
+import Link from 'next/link'
+
+const Show = ({ show, time }) => (
+  <div>
+    <p>This page uses getStaticProps() to pre-fetch a TV show.</p>
+    <p>Ids 1 and 2 are prerendered</p>
+    <hr />
+
+    <h1>Show #{show.id}</h1>
+    <p>{show.name}</p>
+    <p>Rendered at {time} </p>
+    <hr />
+
+    <Link href="/">
+      <a>Go back home</a>
+    </Link>
+  </div>
+)
+
+export async function getStaticPaths() {
+  // Set the paths we want to pre-render
+  const paths = [{ params: { id: '1' } }, { params: { id: '2' } }]
+
+  // We'll pre-render only these paths at build time.
+
+  return { paths, fallback: 'blocking' }
+}
+
+export async function getStaticProps({ params }) {
+  // The ID to render
+  const { id } = params
+
+  const res = await fetch(`https://api.tvmaze.com/shows/${id}`)
+  const data = await res.json()
+  const time = new Date().toLocaleTimeString()
+  return {
+    props: {
+      show: data,
+      time,
+    },
+    revalidate: 60,
+  }
+}
+
+export default Show
