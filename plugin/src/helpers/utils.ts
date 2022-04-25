397ae1b04fbdba8d4926578112ce59bf69da61ea
Erica Pisani <pisani.erica@gmail.com>
2022-04-25 15:42:50 -0400

    style: lint

--- plugin/src/helpers/utils.ts
+++ plugin/src/helpers/utils.ts
@@ -177,7 +177,7 @@ export const findModuleFromBase = ({ paths, candidates }): string | null => {
 
 export const isNextAuthInstalled = (): boolean => {
   try {
-    // eslint-disable-next-line import/no-unassigned-import, import/no-unresolved, node/no-missing-require
+    // eslint-disable-next-line node/no-extraneous-require, import/no-extraneous-dependencies, import/no-unassigned-import
     require('next-auth')
     return true
   } catch {
