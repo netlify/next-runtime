3b9338fb13d9361265ac132fc83aaf67cd4f44e0
Sarah Etter <sarah@sarahetter.com>
2022-04-27 13:25:07 -0400

    feat: add demo for internationalization

---
+++ demos/next-i18next/next.config.js
@@ -0,0 +1,14 @@
+const { i18n } = require('./next-i18next.config')
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  i18n,
+}
+
+module.exports = nextConfig
