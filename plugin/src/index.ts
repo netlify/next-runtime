112a961f8a36c36086c62afa2729b8ba3df70084
Erica Pisani <pisani.erica@gmail.com>
2022-05-02 10:33:01 -0400

    style: lint

--- plugin/src/index.ts
+++ plugin/src/index.ts
@@ -79,7 +79,7 @@ const plugin: NetlifyPlugin = {
       console.log(`NextAuth package detected, setting NEXTAUTH_URL environment variable to ${process.env.URL}`)
 
       const config = await getRequiredServerFiles(publish)
-      const nextAuthUrl = `${process.env.URL}${basePath}`;
+      const nextAuthUrl = `${process.env.URL}${basePath}`
       config.config.env.NEXTAUTH_URL = nextAuthUrl
 
       await updateRequiredServerFiles(publish, config)
