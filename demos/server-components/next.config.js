d9ec00018bffdea1b1116d2976d165c29b0b4bdf
Matt Kane <m@mk.gg>
2022-05-04 21:20:59 +0100

    chore: update RSC demo with new router syntax (#1339)
    * chore: update RSC demo with new router syntax
    
    * chore: fix dependencies
    
    * chore: update demo config
    
    Co-authored-by: Sarah Etter <sarah.etter@netlify.com>
    Co-authored-by: kodiakhq[bot] <49736102+kodiakhq[bot]@users.noreply.github.com>

--- demos/server-components/next.config.js
+++ demos/server-components/next.config.js
@@ -1,4 +1,9 @@
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   experimental: {
     runtime: 'edge',
     serverComponents: true,
