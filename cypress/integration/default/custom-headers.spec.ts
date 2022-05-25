ebcb1050b7ee2242b89e63bccd9e293ff26dc546
Nick Taylor <nick@iamdeveloper.com>
2022-05-25 14:44:57 -0400

    test: some more test for custom headers

--- cypress/integration/default/custom-headers.spec.ts
+++ cypress/integration/default/custom-headers.spec.ts
@@ -1,7 +1,30 @@
 describe('custom headers', () => {
-  it('should load custom headers', () => {
+  it('should load custom headers for an SSG page', () => {
     cy.request('/').then((request) => {
       cy.wrap(request.headers['x-custom-header']).should('equal', 'my custom header value')
+      cy.wrap(request.headers['x-custom-header-for-everything']).should(
+        'equal',
+        'my custom header for everything value',
+      )
+    })
+  })
+
+  it('should load custom headers for a Netlify function', () => {
+    cy.request('/api/hello/').then((request) => {
+      cy.wrap(request.headers['x-custom-api-header']).should('equal', 'my custom api header value')
+      cy.wrap(request.headers['x-custom-header-for-everything']).should(
+        'equal',
+        'my custom header for everything value',
+      )
+    })
+  })
+
+  it('should load custom headers for an SSR page', () => {
+    cy.request('/shows/250').then((request) => {
+      cy.wrap(request.headers['x-custom-header-for-everything']).should(
+        'equal',
+        'my custom header for everything value',
+      )
     })
   })
 })
