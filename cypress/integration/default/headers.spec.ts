deb269a1475504e86232195935e0ea511f2595b6
Nick Taylor <nick@iamdeveloper.com>
2022-05-27 13:34:44 -0400

    test: removed redundant test

--- cypress/integration/default/headers.spec.ts
+++
@@ -1,9 +0,0 @@
-describe('Headers', () => {
-  it('should set headers from the next.config.js', () => {
-    cy.request({
-      url: '/',
-    }).then((response) => {
-      expect(response.headers).to.have.property('x-custom-header', 'my custom header value')
-    })
-  })
-})
