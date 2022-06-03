669a7b0c36ed70a071d0e8772400716069707863
Sarah Etter <sarah.etter@netlify.com>
2022-06-03 13:26:27 -0400

    feat: add canary demo site

---
+++ demos/canary/next.config.js
@@ -0,0 +1,14 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['raw.githubusercontent.com'],
+  },
+}
+
+module.exports = nextConfig
