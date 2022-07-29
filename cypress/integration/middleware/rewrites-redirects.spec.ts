328a58459207ed0da917fcf6e0d6062b82fb2246
Matt Kane <m@mk.gg>
2022-07-29 11:32:47 +0100

    ci: add tests for middleware headers

--- cypress/integration/middleware/rewrites-redirects.spec.ts
+++ cypress/integration/middleware/rewrites-redirects.spec.ts
@@ -2,8 +2,7 @@ describe('Rewrites and Redirects', () => {
   it('rewrites to internal page', () => {
     // preview mode is off by default
     cy.visit('/shows/rewriteme')
-    cy.get('h1').should('contain', 'Shows #100')
-    cy.findByText('NextJS on Netlify (imported Header component)')
+    cy.get('h1').should('contain', 'Show #100')
     cy.url().should('eq', `${Cypress.config().baseUrl}/shows/rewriteme`)
   })
 
@@ -11,5 +10,13 @@ describe('Rewrites and Redirects', () => {
     cy.visit('/shows/rewrite-external')
     cy.get('h1').should('contain', 'Example Domain')
     cy.url().should('eq', `${Cypress.config().baseUrl}/shows/rewrite-external`)
+  })
+
+  it('adds headers to static pages', () => {
+    cy.request('/shows/static/3').then((response) => {
+      expect(response.headers).to.have.property('x-middleware-date')
+      expect(response.headers).to.have.property('x-is-deno', 'true')
+      expect(response.headers).to.have.property('x-modified-edge', 'true')
+    })
   })
 })
