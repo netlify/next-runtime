689b2ac055f268c99415dd8a4fe5ddd5db2d5598
Matt Kane <matt.kane@netlify.com>
2022-07-30 07:37:28 +0100

    feat: add enhanced middleware support (#1479)
    * fix: update patch syntax
    
    * feat: add support for rewriting middleware responses
    
    * chore: format
    
    * chore: add extra content
    
    * chore: use city in demo
    
    * feat: add html rewriting
    
    * feat: add request header support
    
    * feat: add rewriting
    
    * chore: move NetlifyReponse into a subpackage
    
    * feat: allow returning `NetlifyReponse` directly
    
    * chore: remove inlined types from middleware demo
    
    * chore: remove modified toml
    
    * chore: add demo links
    
    * chore: add comments to example
    
    * chore: don't lint generated types
    
    * chore: rename class
    
    * chore: add comment about source of htmlrewriter types
    
    * refactor: use type guards
    
    * chore: rename classes
    
    * chore: rename again
    
    * chore: rename again
    
    * chore: update example
    
    * chore: make req a subclass of Request
    
    * chore: switch from hidden fields to global map
    
    * ci: add cypress middleware tests
    
    * ci: add tests for middleware headers
    
    * ci: add tests for enhanced middleware
    
    * chore: fix test
    
    * fix: handle other HTTP verbs
    
    * feat: add helper methods
    
    * fix: less flaky test
    
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

---
+++ cypress/integration/middleware/enhanced.spec.ts
@@ -0,0 +1,28 @@
+describe('Enhanced middleware', () => {
+  it('adds request headers', () => {
+    cy.request('/api/hello').then((response) => {
+      expect(response.body).to.have.nested.property('headers.x-hello', 'world')
+    })
+  })
+
+  it('adds request headers to a rewrite', () => {
+    cy.request('/headers').then((response) => {
+      expect(response.body).to.have.nested.property('headers.x-hello', 'world')
+    })
+  })
+
+  it('rewrites the response body', () => {
+    cy.visit('/static')
+    cy.get('#message').contains('This was static but has been transformed in')
+    cy.contains("This is an ad that isn't shown by default")
+  })
+
+  it('modifies the page props', () => {
+    cy.request('/_next/data/build-id/static.json').then((response) => {
+      expect(response.body).to.have.nested.property('pageProps.showAd', true)
+      expect(response.body)
+        .to.have.nested.property('pageProps.message')
+        .that.includes('This was static but has been transformed in')
+    })
+  })
+})
--- plugin/src/templates/edge/ipx.ts
+++ plugin/src/templates/edge/ipx.ts
@@ -1,14 +1,12 @@
-import { Accepts } from "https://deno.land/x/accepts@2.1.1/mod.ts";
-import type { Context } from "netlify:edge";
+import { Accepts } from 'https://deno.land/x/accepts@2.1.1/mod.ts'
+import type { Context } from 'netlify:edge'
 // Available at build time
-import imageconfig from "./imageconfig.json" assert {
-  type: "json",
-};
+import imageconfig from './imageconfig.json' assert { type: 'json' }
 
-const defaultFormat = "webp"
+const defaultFormat = 'webp'
 
 interface ImageConfig extends Record<string, unknown> {
-  formats?: string[];
+  formats?: string[]
 }
 
 /**
@@ -17,41 +15,38 @@ interface ImageConfig extends Record<string, unknown> {
 
 // deno-lint-ignore require-await
 const handler = async (req: Request, context: Context) => {
-  const { searchParams } = new URL(req.url);
-  const accept = new Accepts(req.headers);
-  const { formats = [defaultFormat] } = imageconfig as ImageConfig;
+  const { searchParams } = new URL(req.url)
+  const accept = new Accepts(req.headers)
+  const { formats = [defaultFormat] } = imageconfig as ImageConfig
   if (formats.length === 0) {
-    formats.push(defaultFormat);
+    formats.push(defaultFormat)
   }
-  let type = accept.types(formats) || defaultFormat;
-  if(Array.isArray(type)) {
-    type = type[0];
+  let type = accept.types(formats) || defaultFormat
+  if (Array.isArray(type)) {
+    type = type[0]
   }
 
-
-  const source = searchParams.get("url");
-  const width = searchParams.get("w");
-  const quality = searchParams.get("q") ?? 75;
+  const source = searchParams.get('url')
+  const width = searchParams.get('w')
+  const quality = searchParams.get('q') ?? 75
 
   if (!source || !width) {
-    return new Response("Invalid request", {
+    return new Response('Invalid request', {
       status: 400,
-    });
+    })
   }
 
-  const modifiers = [`w_${width}`, `q_${quality}`];
+  const modifiers = [`w_${width}`, `q_${quality}`]
 
   if (type) {
-    if(type.includes('/')) {
+    if (type.includes('/')) {
       // If this is a mimetype, strip "image/"
-      type = type.split('/')[1];
+      type = type.split('/')[1]
     }
-    modifiers.push(`f_${type}`);
+    modifiers.push(`f_${type}`)
   }
-  const target = `/_ipx/${modifiers.join(",")}/${encodeURIComponent(source)}`;
-  return context.rewrite(
-    target,
-  );
-};
+  const target = `/_ipx/${modifiers.join(',')}/${encodeURIComponent(source)}`
+  return context.rewrite(target)
+}
 
-export default handler;
+export default handler
