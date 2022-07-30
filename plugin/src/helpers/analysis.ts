25f3e2fb82618d89c22b46ebabdcfb651d752b94
Matt Kane <m@mk.gg>
2022-07-30 11:19:40 +0100

    feat: add static analysis helper

---
+++ plugin/src/helpers/analysis.ts
@@ -0,0 +1,21 @@
+import fs from 'fs'
+import { relative } from 'path'
+
+import { extractExportedConstValue, UnsupportedValueError } from 'next/dist/build/analysis/extract-const-value'
+import { parseModule } from 'next/dist/build/analysis/parse-module'
+
+export const extractConfigFromFile = async (apiFilePath: string): Promise<Record<string, unknown>> => {
+  const fileContent = await fs.promises.readFile(apiFilePath, 'utf8')
+  if (!fileContent.includes('config')) {
+    return {}
+  }
+  const ast = await parseModule(apiFilePath, fileContent)
+  try {
+    return extractExportedConstValue(ast, 'config')
+  } catch (error) {
+    if (error instanceof UnsupportedValueError) {
+      console.warn(`Unsupported config value in ${relative(process.cwd(), apiFilePath)}`)
+    }
+    return {}
+  }
+}
---
+++ test/analysis.spec.ts
@@ -0,0 +1,32 @@
+import { extractConfigFromFile } from '../plugin/src/helpers/analysis'
+import { resolve } from 'path'
+describe('static source analysis', () => {
+  it('should extract config values from a source file', async () => {
+    const config = await extractConfigFromFile(resolve(__dirname, './fixtures/analysis/background.js'))
+    expect(config).toEqual({
+      background: true,
+    })
+  })
+  it('should extract config values from a TypeScript source file', async () => {
+    const config = await extractConfigFromFile(resolve(__dirname, './fixtures/analysis/background.ts'))
+    expect(config).toEqual({
+      background: true,
+    })
+  })
+  it('should return an empty config if not defined', async () => {
+    const config = await extractConfigFromFile(resolve(__dirname, './fixtures/analysis/missing.ts'))
+    expect(config).toEqual({})
+  })
+
+  it('should return an empty config if config is invalid', async () => {
+    const config = await extractConfigFromFile(resolve(__dirname, './fixtures/analysis/invalid.ts'))
+    expect(config).toEqual({})
+  })
+
+  it('should extract schedule values from a source file', async () => {
+    const config = await extractConfigFromFile(resolve(__dirname, './fixtures/analysis/scheduled.ts'))
+    expect(config).toEqual({
+      schedule: '@daily',
+    })
+  })
+})
