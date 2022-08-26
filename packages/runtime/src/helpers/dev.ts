b0c33989df2a5401540ad435a90247ef1d13f276
Erica Pisani <pisani.erica@gmail.com>
2022-08-26 09:54:34 -0400

    feat: improve messaging on middleware detection

--- packages/runtime/src/helpers/dev.ts
+++ packages/runtime/src/helpers/dev.ts
@@ -19,22 +19,28 @@ export const onPreDev: OnPreBuild = async ({ constants, netlifyConfig }) => {
     // Ignore if it doesn't exist
   })
   await writeDevEdgeFunction(constants)
+
+  let middlewareDetected = false
   if (!existsSync(resolve(base, 'middleware.ts')) && !existsSync(resolve(base, 'middleware.js'))) {
     console.log(
-      "No middleware found. Create a 'middleware.ts' or 'middleware.js' file in your project root to add custom middleware.",
+      "No middleware found. If you did intend to use middleware, create a 'middleware.ts' or 'middleware.js' file in your project root to add custom middleware.",
     )
   } else {
     console.log('Watching for changes in Next.js middleware...')
+    middlewareDetected = true
   }
   // Eventually we might want to do this via esbuild's API, but for now the CLI works fine
 
   const common = [`--bundle`, `--outdir=${resolve('.netlify')}`, `--format=esm`, `--target=esnext`, '--watch']
 
+  const esbuildTSArgs = middlewareDetected ? [...common, resolve(base, 'middleware.ts')] : common
+  const esbuildJSArgs = middlewareDetected ? [...common, resolve(base, 'middleware.js')] : common
+
   // TypeScript
-  execa(`esbuild`, [...common, resolve(base, 'middleware.ts')], { all: true }).all.pipe(process.stdout)
+  execa(`esbuild`, esbuildTSArgs, { all: true }).all.pipe(process.stdout)
 
   // JavaScript
-  execa(`esbuild`, [...common, resolve(base, 'middleware.js')], { all: true }).all.pipe(process.stdout)
+  execa(`esbuild`, esbuildJSArgs, { all: true }).all.pipe(process.stdout)
 
   // Don't return the promise because we don't want to wait for the child process to finish
 }
