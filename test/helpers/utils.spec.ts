fd57ac557c948725c5367fe2cd41bc1f203088cb
Erica Pisani <pisani.erica@gmail.com>
2022-09-08 13:13:52 -0400

    feat: support updated remotePatterns configuration

--- test/helpers/utils.spec.ts
+++ test/helpers/utils.spec.ts
@@ -1,5 +1,7 @@
 import Chance from 'chance'
-import { getCustomImageResponseHeaders } from '../../packages/runtime/src/helpers/utils'
+import { ExperimentalConfig } from 'next/dist/server/config-shared'
+import { ImageConfigComplete } from 'next/dist/shared/lib/image-config'
+import { getCustomImageResponseHeaders, getRemotePatterns, ImagesConfig } from '../../packages/runtime/src/helpers/utils'
 
 const chance = new Chance()
 
@@ -29,5 +31,65 @@ describe('getCustomImageResponseHeaders', () => {
     expect(result).toStrictEqual({
       'X-Foo': mockFooValue,
     })
+  })
+})
+
+describe('getRemotePatterns', () => {
+  let mockExperimentalConfig
+  let mockImages
+  beforeEach(() => {
+    mockExperimentalConfig = {
+      images: {}
+    } as ExperimentalConfig
+  
+    mockImages = {
+      deviceSizes: [
+        640,  750,  828,
+        1080, 1200, 1920,
+        2048, 3840
+      ],
+      imageSizes: [
+        16,  32,  48,  64,
+        96, 128, 256, 384
+      ],
+      path: '/_next/image',
+      loader: 'default',
+      domains: [],
+      disableStaticImages: false,
+      minimumCacheTTL: 60,
+      formats: [ 'image/avif', 'image/webp' ],
+      dangerouslyAllowSVG: false,
+      contentSecurityPolicy: "script-src 'none'; frame-src 'none'; sandbox;",
+      unoptimized: false
+    } as ImagesConfig
+  
+  })
+
+  it('returns the remote patterns found under experimental.images', () => {
+    mockExperimentalConfig.images.remotePatterns = [
+      {
+        protocol: 'https',
+        hostname: '*.githubusercontent.com',
+      },
+    ]
+    const result = getRemotePatterns(mockExperimentalConfig, mockImages)
+  
+    expect(result).toStrictEqual(mockExperimentalConfig.images?.remotePatterns)
+  })
+
+  it('returns the remote patterns found under images', () => {
+    mockImages.remotePatterns = [
+      {
+        protocol: 'https',
+        hostname: '*.githubusercontent.com',
+      },
+    ]
+    const result = getRemotePatterns(mockExperimentalConfig, mockImages)
+  
+    expect(result).toStrictEqual(mockExperimentalConfig.images?.remotePatterns)  
+  })
+  it('returns an empty array', () => {
+    const result = getRemotePatterns(mockExperimentalConfig, mockImages)
+    expect(result).toStrictEqual([])
   })
 })
