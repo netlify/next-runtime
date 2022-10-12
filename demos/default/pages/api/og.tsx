91b05a51bd60f1b904620e984e37743404e6ec06
Matt Kane <m@mk.gg>
2022-10-12 11:58:36 +0100

    chore: nicer example

--- demos/default/pages/api/og.tsx
+++ demos/default/pages/api/og.tsx
@@ -1,29 +1,50 @@
 import { ImageResponse } from '@vercel/og'
+import { NextRequest } from 'next/server'
 
 export const config = {
   runtime: 'experimental-edge',
 }
-export default function () {
+
+export default async function handler(req: NextRequest) {
+  const { searchParams } = req.nextUrl
+  const username = searchParams.get('username')
+  if (!username) {
+    return new ImageResponse(<>Visit with &quot;?username=netlify&quot;</>, {
+      width: 1200,
+      height: 630,
+    })
+  }
+
   return new ImageResponse(
     (
       <div
         style={{
-          fontSize: 128,
-          background: 'white',
+          display: 'flex',
+          fontSize: 60,
+          color: 'black',
+          background: '#f6f6f6',
           width: '100%',
           height: '100%',
-          display: 'flex',
-          textAlign: 'center',
-          alignItems: 'center',
+          paddingTop: 50,
+          flexDirection: 'column',
           justifyContent: 'center',
+          alignItems: 'center',
         }}
       >
-        Hi, it's {new Date().toLocaleTimeString()}!
+        <img
+          width="256"
+          height="256"
+          src={`https://github.com/${username}.png`}
+          style={{
+            borderRadius: 128,
+          }}
+        />
+        <p>github.com/{username}</p>
       </div>
     ),
     {
       width: 1200,
-      height: 600,
+      height: 630,
     },
   )
 }
