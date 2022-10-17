a4ccc000fda44fbf507241a874765deceb15f9af
Erica Pisani <pisani.erica@gmail.com>
2022-10-17 14:32:40 -0400

    test: test

--- demos/default/pages/_app.tsx
+++ demos/default/pages/_app.tsx
@@ -2,7 +2,7 @@ import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   const getLayout = Component.getLayout || ((page) => page)
-
+  console.log('IN APP')
   return (
     <div className='prose ml-14 mt-10 mb-20'>
       {getLayout(<Component {...pageProps} />)}
