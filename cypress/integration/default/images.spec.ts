1de22f8e9811ebe7259f168b26f8124132d259d3
Nick Taylor <nick@iamdeveloper.com>
2022-10-27 14:19:15 -0400

    chore: removed broken image that was no longer broken from main images demo page

--- cypress/integration/default/images.spec.ts
+++ cypress/integration/default/images.spec.ts
@@ -4,13 +4,12 @@
 describe('next/images', () => {
   it('should show static image from /public', () => {
     cy.visit('/')
-    cy.findByRole('img').should('be.visible').and(($img) => {
-      // "naturalWidth" and "naturalHeight" are set when the image loads
-      expect(
-        $img[0].naturalWidth,
-        'image has natural width'
-      ).to.be.greaterThan(0)
-    })
+    cy.findByRole('img')
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth, 'image has natural width').to.be.greaterThan(0)
+      })
   })
 
   it('should show image using next/image', () => {
@@ -20,24 +19,11 @@ describe('next/images', () => {
 
   it('should show image allow-listed with remotePatterns', () => {
     cy.visit('/image')
-    cy.findByRole('img',{ name: /tawny frogmouth/i }).should('be.visible').and(($img) => {
-      // "naturalWidth" and "naturalHeight" are set when the image loads
-      expect(
-        $img[0].naturalWidth,
-        'image has natural width'
-      ).to.be.greaterThan(0)
-    })
-  })
-
-  it('should show a broken image if it is not on domains or remotePatterns allowlist', () => {
-    cy.visit('/image')
-    cy.findByRole('img',{ name: /jellybeans/i }).should('be.visible').and(($img) => {
-      expect(
-        $img[0].style.height
-      ).to.equal('0px')
-      expect(
-        $img[0].style.width
-      ).to.equal('0px')
-    })
+    cy.findByRole('img', { name: /tawny frogmouth/i })
+      .should('be.visible')
+      .and(($img) => {
+        // "naturalWidth" and "naturalHeight" are set when the image loads
+        expect($img[0].naturalWidth, 'image has natural width').to.be.greaterThan(0)
+      })
   })
 })
