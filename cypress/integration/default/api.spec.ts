da8f4405a1f7fd3da6789743f93400d678109022
Matt Kane <matt.kane@netlify.com>
2022-10-27 16:55:04 +0100

    fix: disable minimal mode for API routes (#1727)

--- cypress/integration/default/api.spec.ts
+++ cypress/integration/default/api.spec.ts
@@ -1,3 +1,11 @@
+describe('API routes', () => {
+  it('serves custom headers on an api route', () => {
+    cy.request('/api/hello').then((response) => {
+      expect(response.headers).to.have.property('x-custom-api-header', 'my custom api header value')
+    })
+  })
+})
+
 describe('Extended API routes', () => {
   it('returns HTTP 202 Accepted for background route', () => {
     cy.request('/api/hello-background').then((response) => {
