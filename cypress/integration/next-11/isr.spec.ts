3402c0f7e0a67389b33db0a89b0b5d5789ff677c
Rob Stanford <me@robstanford.com>
2022-11-14 21:37:43 +0000

    test: update 404 isr test to not fail on 404

--- cypress/integration/next-11/isr.spec.ts
+++ cypress/integration/next-11/isr.spec.ts
@@ -1,6 +1,6 @@
 describe('ISR pages', () => {
   it('renders a 404 from an ISR page with TTL=60', () => {
-    cy.request('/isr-not-found').then((res) => {
+    cy.request({ url: '/isr-not-found', failOnStatusCode: false }).then((res) => {
       expect(res.status).to.eq(404)
       expect(res.headers).to.have.property('x-nf-render-mode', 'odb ttl=60')
     })
