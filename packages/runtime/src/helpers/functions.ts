aaefe226130a6afea972e62691c46e7ccc55f11d
Matt Kane <m@mk.gg>
2022-11-16 15:43:14 +0000

    chore: simplify function signature

--- packages/runtime/src/helpers/functions.ts
+++ packages/runtime/src/helpers/functions.ts
@@ -80,10 +80,10 @@ export const generateFunctions = async (
  * This is just so that the nft bundler knows about them. We'll eventually do this better.
  */
 export const generatePagesResolver = async ({
-  constants: { INTERNAL_FUNCTIONS_SRC, FUNCTIONS_SRC = DEFAULT_FUNCTIONS_SRC, PUBLISH_DIR },
-}: {
-  constants: NetlifyPluginConstants
-}): Promise<void> => {
+  INTERNAL_FUNCTIONS_SRC,
+  FUNCTIONS_SRC = DEFAULT_FUNCTIONS_SRC,
+  PUBLISH_DIR,
+}: NetlifyPluginConstants): Promise<void> => {
   const functionsPath = INTERNAL_FUNCTIONS_SRC || FUNCTIONS_SRC
 
   const jsSource = await getResolverForPages(PUBLISH_DIR)
