bcb90bfd966eb9ca27ab8b629fdbdcb16e9e0230
Matt Kane <m@mk.gg>
2022-12-01 11:37:05 +0000

    fix: assign globals to `self`

--- packages/runtime/src/helpers/edge.ts
+++ packages/runtime/src/helpers/edge.ts
@@ -85,8 +85,6 @@ import {
 // Deno defines "window", but naughty libraries think this means it's a browser
 delete globalThis.window
 globalThis.process = { env: {...Deno.env.toObject(), NEXT_RUNTIME: 'edge', 'NEXT_PRIVATE_MINIMAL_MODE': '1' } }
-// Next uses "self" as a function-scoped global-like object
-const self = {}
 let _ENTRIES = {}
 
 // Next.js uses this extension to the Headers API implemented by Cloudflare workerd
@@ -117,6 +115,9 @@ const fetch = async (url, init) => {
     throw error
   }
 }
+
+// Latest Next uses "self" as a function-scoped global-like object, but older versions expect it to equal "globalThis"
+const self = { ...globalThis, fetch }
 
 `
 
