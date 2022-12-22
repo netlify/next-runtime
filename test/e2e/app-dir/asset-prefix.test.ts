bfec6350f3226310dd0f0b4c7b3b63ef90be60c7
Matt Kane <m@mk.gg>
2022-12-22 15:24:08 +0000

    chore: fix test

--- test/e2e/app-dir/asset-prefix.test.ts
+++ test/e2e/app-dir/asset-prefix.test.ts
@@ -36,9 +36,7 @@ describe('app-dir assetPrefix handling', () => {
         redirect: 'manual',
       },
     )
-    // NTL - uses 301 instead of 308
-    expect(res.status).toBe(301)
-    // expect(res.status).toBe(308)
+    expect(res.status).toBe(308)
     expect(res.headers.get('location')).toBe(next.url + '/a')
   })
 
