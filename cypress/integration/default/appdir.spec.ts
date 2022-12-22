47ca397973c50b138f7417af3169c919e56fcac6
Matt Kane <m@mk.gg>
2022-12-22 14:56:21 +0000

    test: add e2e test

---
+++ cypress/integration/default/appdir.spec.ts
@@ -0,0 +1,19 @@
+describe('appDir', () => {
+  it('renders appdir pages using SSR', () => {
+    cy.request('/blog/erica/first-post/').then((response) => {
+      expect(response.headers).to.have.property('x-nf-render-mode', 'ssr')
+      expect(response.headers).to.have.property('context-type', 'text/html; charset=utf-8')
+    })
+  })
+
+  it('returns RSC data for RSC requests', () => {
+    cy.request('/blog/erica/first-post/', {
+      Headers: {
+        RSC: '1',
+      },
+    }).then((response) => {
+      expect(response.headers).to.have.property('x-nf-render-mode', 'ssr')
+      expect(response.headers).to.have.property('context-type', 'application/octet-stream')
+    })
+  })
+})
