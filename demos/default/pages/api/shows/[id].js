453605c38f4ecf953de32ef5990467eeb64d9ef4
Rob Stanford <me@robstanford.com>
2023-01-04 16:32:16 +0000

    chore: validate user input for tv maze urls

--- demos/default/pages/api/shows/[id].js
+++ demos/default/pages/api/shows/[id].js
@@ -7,7 +7,7 @@ export default async (req, res) => {
   const { id } = query
 
   // Get the data
-  const fetchRes = await fetch(`https://tvproxy.netlify.app/shows/${id}`)
+  const fetchRes = await fetch(`https://tvproxy.netlify.app/shows/${Number(id)}`)
   const data = await fetchRes.json()
 
   // If show was found, return it
