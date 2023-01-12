cc1cc9a177f78702dc7e73d4415de4c17ebeb2c1
Matt Kane <m@mk.gg>
2023-01-12 16:32:46 +0000

    test: add more cypress tests

--- cypress/integration/default/appdir.spec.ts
+++ cypress/integration/default/appdir.spec.ts
@@ -1,11 +1,34 @@
 describe('appDir', () => {
-  it('renders appdir pages as HTML by default', () => {
+  it('renders ISR appdir pages as HTML by default', () => {
+    cy.request('/blog/erica/').then((response) => {
+      expect(response.headers['content-type']).to.match(/^text\/html/)
+    })
+  })
+
+  it('renders static appdir pages as HTML by default', () => {
     cy.request('/blog/erica/first-post/').then((response) => {
       expect(response.headers['content-type']).to.match(/^text\/html/)
     })
   })
 
-  it('returns RSC data for RSC requests', () => {
+  it('renders dynamic appdir pages as HTML by default', () => {
+    cy.request('/blog/erica/random-post/').then((response) => {
+      expect(response.headers['content-type']).to.match(/^text\/html/)
+    })
+  })
+
+  it('returns RSC data for RSC requests to ISR pages', () => {
+    cy.request({
+      url: '/blog/erica/',
+      headers: {
+        RSC: '1',
+      },
+    }).then((response) => {
+      expect(response.headers).to.have.property('content-type', 'application/octet-stream')
+    })
+  })
+
+  it('returns RSC data for RSC requests to static pages', () => {
     cy.request({
       url: '/blog/erica/first-post/',
       headers: {
@@ -13,6 +36,39 @@ describe('appDir', () => {
       },
     }).then((response) => {
       expect(response.headers).to.have.property('content-type', 'application/octet-stream')
+    })
+  })
+
+  it('returns RSC data for RSC requests to dynamic pages', () => {
+    cy.request({
+      url: '/blog/erica/random-post/',
+      headers: {
+        RSC: '1',
+      },
+    }).then((response) => {
+      expect(response.headers).to.have.property('content-type', 'application/octet-stream')
+    })
+  })
+
+  it('correctly redirects HTML requests for ISR pages', () => {
+    cy.request('/blog/erica').then((response) => {
+      expect(response.status).to.be('308')
+      expect(response.headers).to.have.property('location', '/blog/erica/')
+    })
+  })
+
+  // This needs trailing slash handling to be fixed
+  it.skip('correctly redirects HTML requests for static pages', () => {
+    cy.request('/blog/erica/first-post').then((response) => {
+      expect(response.status).to.be('308')
+      expect(response.headers).to.have.property('location', '/blog/erica/first-post/')
+    })
+  })
+
+  it('correctly redirects HTML requests for dynamic pages', () => {
+    cy.request('/blog/erica/random-post').then((response) => {
+      expect(response.status).to.be('308')
+      expect(response.headers).to.have.property('location', '/blog/erica/random-post/')
     })
   })
 })
