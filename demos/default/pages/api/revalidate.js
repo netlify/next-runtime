0f7d1dbb920413af9cdda9aa3ec12c1b6c070e94
Rob Stanford <me@robstanford.com>
2023-03-06 12:47:02 +0000

    fix: revalidate error messaging in demo

--- demos/default/pages/api/revalidate.js
+++ demos/default/pages/api/revalidate.js
@@ -3,8 +3,8 @@ export default async function handler(req, res) {
     const path = '/getStaticProps/with-revalidate/'
     await res.revalidate(path)
     console.log('Revalidated:', path)
-    return res.json({ revalidated: true })
+    return res.json({ code: 200, message: 'success' })
   } catch (err) {
-    return res.status(500).send('Error revalidating:', err)
+    return res.status(500).send({ code: 200, message: err.message })
   }
 }
