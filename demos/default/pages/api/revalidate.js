9fc412329371116f1d882e9f0b78c633377fe8c6
Rob Stanford <me@robstanford.com>
2023-03-17 13:04:32 +0000

    chore: update revalidate test paths

--- demos/default/pages/api/revalidate.js
+++ demos/default/pages/api/revalidate.js
@@ -4,17 +4,13 @@ export default async function handler(req, res) {
 
   const paths = [
     '/getStaticProps/with-revalidate/', // valid path
-    '/getStaticProps/with-revalidate', // missing trailing slash
-    'getStaticProps/with-revalidate', // missing leading/trailing slash
-    '/en/getStaticProps/with-revalidate/', // valid path (with locale)
     '/fr/getStaticProps/with-revalidate/', // valid path (with locale)
     '/', // valid path (index)
-    '/en', // missing trailing slash (index)
     '/fr/', // valid path (index with locale)
-    '/nothing-here/', // 404
     '/getStaticProps/withRevalidate/2/', // valid path (with dynamic route)
     '/getStaticProps/withRevalidate/3/', // invalid path (fallback false with dynamic route)
     '/getStaticProps/withRevalidate/withFallbackBlocking/3/', // valid path (fallback blocking with dynamic route)
+    '/fr/getStaticProps/withRevalidate/withFallbackBlocking/3/', // valid path (fallback blocking with dynamic route and locale)
     '/blog/nick/', // valid path (with appDir dynamic route)
     '/blog/greg/', // invalid path (with appDir dynamic route)
     '/blog/rob/hello/', // valid path (with appDir dynamic route)
