082704f155547cff314f040f835de189e445e542
Nick Taylor <nick@iamdeveloper.com>
2023-03-16 20:16:20 -0400

    test: added more tests for writeFunctionConfiguration

--- test/functions.spec.ts
+++ test/functions.spec.ts
@@ -5,9 +5,50 @@ import { NEXT_PLUGIN_NAME } from '../packages/runtime/src/constants'
 import { writeFunctionConfiguration } from '../packages/runtime/src/helpers/utilities/functions'
 
 describe('writeFunctionConfiguration', () => {
-  const nextRuntimeVersion = '29.3.4'
+  afterEach(() => {
+    mock.restore()
+  })
 
-  beforeEach(() => {
+  it('should write the configuration for a function using node modules version of @netlify/plugin-nextjs', async () => {
+    const nextRuntimeVersion = '23.4.5'
+
+    mock({
+      '.netlify/plugins/package.json': JSON.stringify({
+        name: 'test',
+        version: '1.0.0',
+        dependencies: {
+          '@netlify/plugin-nextjs': '29.3.4',
+        },
+      }),
+      'node_modules/@netlify/plugin-nextjs/package.json': JSON.stringify({
+        name: '@netlify/plugin-nextjs',
+        version: nextRuntimeVersion,
+      }),
+      '.netlify/functions/some-folder/someFunctionName/someFunctionName.json': '',
+    })
+
+    const functionName = 'someFunctionName'
+    const functionTitle = 'some function title'
+    const functionsDir = '.netlify/functions/some-folder'
+
+    const expected = {
+      config: {
+        name: functionTitle,
+        generator: `${NEXT_PLUGIN_NAME}@${nextRuntimeVersion}`,
+      },
+      version: 1,
+    }
+
+    const filePathToSaveTo = join(functionsDir, functionName, `${functionName}.json`)
+    await writeFunctionConfiguration(functionName, functionTitle, functionsDir)
+    const actual = await readJSON(filePathToSaveTo)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should write the configuration for a function using version of @netlify/plugin-nextjs in package.json', async () => {
+    const nextRuntimeVersion = '23.4.5'
+
     mock({
       '.netlify/plugins/package.json': JSON.stringify({
         name: 'test',
@@ -18,13 +59,7 @@ describe('writeFunctionConfiguration', () => {
       }),
       '.netlify/functions/some-folder/someFunctionName/someFunctionName.json': '',
     })
-  })
 
-  afterEach(() => {
-    mock.restore()
-  })
-
-  it('should write the configuration for a function', async () => {
     const functionName = 'someFunctionName'
     const functionTitle = 'some function title'
     const functionsDir = '.netlify/functions/some-folder'
@@ -32,7 +67,31 @@ describe('writeFunctionConfiguration', () => {
     const expected = {
       config: {
         name: functionTitle,
-        generator: nextRuntimeVersion ? `${NEXT_PLUGIN_NAME}@${nextRuntimeVersion}` : 'Next Runtime Version Not Found',
+        generator: `${NEXT_PLUGIN_NAME}@${nextRuntimeVersion}`,
+      },
+      version: 1,
+    }
+
+    const filePathToSaveTo = join(functionsDir, functionName, `${functionName}.json`)
+    await writeFunctionConfiguration(functionName, functionTitle, functionsDir)
+    const actual = await readJSON(filePathToSaveTo)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should write the configuration for a function with runtime version not found', async () => {
+    mock({
+      '.netlify/functions/some-folder/someFunctionName/someFunctionName.json': '',
+    })
+
+    const functionName = 'someFunctionName'
+    const functionTitle = 'some function title'
+    const functionsDir = '.netlify/functions/some-folder'
+
+    const expected = {
+      config: {
+        name: functionTitle,
+        generator: 'Next Runtime Version Not Found',
       },
       version: 1,
     }
