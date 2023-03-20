6fec83a2fea8c891b8f2ce60aaf89a8d10a81e89
Marc Littlemore <marc.littlemore@netlify.com>
2023-03-20 14:54:07 +0000

    refactor: add onEnd hook and move onPreDev for consistency

---
+++ packages/runtime/src/build/onEnd.ts
@@ -0,0 +1,7 @@
+export const onEnd = ({ utils }) => {
+  utils.status.show({
+    title: 'Please upgrade to the latest version of the Netlify CLI',
+    summary:
+      'To support for the latest Next.js features, please upgrade to the latest version of the Netlify CLI',
+  })
+};
--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -1,8 +1,9 @@
 import type { NetlifyPlugin } from '@netlify/build'
-import { onPreDev } from './helpers/dev'
+import { onPreDev } from './build/onPreDev'
 import { onPreBuild } from './build/onPreBuild'
 import { onBuild } from './build/onBuild'
 import { onPostBuild } from './build/onPostBuild'
+import { onEnd } from './build/onEnd'
 
 const plugin: NetlifyPlugin = {
   onPreBuild,
@@ -17,13 +18,7 @@ const nextRuntime = (
   if (!meta?.events?.has('onPreDev')) {
     return {
       ...plugin,
-      onEnd: ({ utils }) => {
-        utils.status.show({
-          title: 'Please upgrade to the latest version of the Netlify CLI',
-          summary:
-            'To support for the latest Next.js features, please upgrade to the latest version of the Netlify CLI',
-        })
-      },
+      onEnd
     }
   }
   return {
