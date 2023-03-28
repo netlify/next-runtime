acbc2716228d8c270d4401f63ffb2eba44124d53
Nick Taylor <nick@iamdeveloper.com>
2023-03-28 08:51:15 -0400

    chore: made a comment more comprehensive

--- test/rsc-data.spec.ts
+++ test/rsc-data.spec.ts
@@ -28,7 +28,9 @@ describe('getRscDataRouter', () => {
     let rscDataRouter
 
     // Normally type checking would pick this up, but because this file is copied when generating
-    // edge functions for the build, I added a test
+    // edge functions for the build, we need to make sure it's valid for builds.
+    //
+    // See https://github.com/netlify/next-runtime/issues/1940
     expect(() => {
       rscDataRouter = getRscDataRouter(manifest)
     }).not.toThrow()
