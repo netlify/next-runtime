cfebd772f08d27374339d8881812d0efcf26cec4
taty2010 <shydalys123@gmail.com>
2023-03-30 17:11:22 -0500

    feat: added testing for edge function generator

--- test/functionsMetaData.spec.ts
+++ test/functionsMetaData.spec.ts
@@ -1,8 +1,9 @@
-import { readJSON } from 'fs-extra'
+import { readJSON, writeJSON } from 'fs-extra'
 import mock from 'mock-fs'
 import { join } from 'pathe'
 import { NEXT_PLUGIN_NAME } from '../packages/runtime/src/constants'
 import { writeFunctionConfiguration } from '../packages/runtime/src/helpers/functionsMetaData'
+import { writeEdgeFunctionConfiguration } from '../packages/runtime/src/helpers/functionsMetaData'
 
 describe('writeFunctionConfiguration', () => {
   afterEach(() => {
@@ -17,7 +18,7 @@ describe('writeFunctionConfiguration', () => {
         name: 'test',
         version: '1.0.0',
         dependencies: {
-          '@netlify/plugin-nextjs': '29.3.4',
+          // '@netlify/plugin-nextjs': '29.3.4',
         },
       }),
       'node_modules/@netlify/plugin-nextjs/package.json': JSON.stringify({
@@ -103,3 +104,160 @@ describe('writeFunctionConfiguration', () => {
     expect(actual).toEqual(expected)
   })
 })
+
+// EDGE FUNCTIONS
+
+describe('writeEdgeFunctionConfiguration', () => {
+  afterEach(() => {
+    mock.restore()
+  })
+
+  it('should write the configuration for an edge function using node modules version of @netlify/plugin-nextjs', async () => {
+    const nextRuntimeVersion = '23.4.5'
+
+    mock({
+      '.netlify/plugins/package.json': JSON.stringify({
+        name: 'test',
+        version: '1.0.0',
+        dependencies: {
+          '@netlify/plugin-nextjs': '29.3.4',
+        },
+      }),
+      'node_modules/@netlify/plugin-nextjs/package.json': JSON.stringify({
+        name: '@netlify/plugin-nextjs',
+        version: nextRuntimeVersion,
+      }),
+      '.netlify/edge-functions/manifest.json': JSON.stringify({
+        "functions":[],
+        "version":1
+      }),
+    })
+
+    const manifest = {
+    functions:[],
+    version:1
+    }
+
+    const functionName = 'someFunction'
+    const name = 'someFunctionName'
+
+    const pluginVersion = await writeEdgeFunctionConfiguration()
+
+    manifest.functions.push({
+      functions: functionName,
+      name: name,
+      generator: pluginVersion
+    })
+
+    const expected = {
+      functions: [
+        {
+          functions: functionName,
+          name: name,
+          generator: `${NEXT_PLUGIN_NAME}@${nextRuntimeVersion}`,
+        }
+    ],
+      version: 1,
+    }
+
+    const filePathToSaveTo = '.netlify/edge-functions/manifest.json'
+    await writeJSON(filePathToSaveTo, manifest)
+    const actual = await readJSON(filePathToSaveTo)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should write the configuration for a function using version of @netlify/plugin-nextjs in package.json', async () => {
+    const nextRuntimeVersion = '23.4.5'
+
+    mock({
+      '.netlify/plugins/package.json': JSON.stringify({
+        name: 'test',
+        version: '1.0.0',
+        dependencies: {
+          '@netlify/plugin-nextjs': nextRuntimeVersion,
+        },
+      }),
+      '.netlify/edge-functions/manifest.json': JSON.stringify({
+        "functions":[],
+        "version":1
+      }),
+    })
+
+    const manifest = {
+      functions:[],
+      version:1
+      }
+  
+      const functionName = 'someFunction'
+      const name = 'someFunctionName'
+  
+      const pluginVersion = await writeEdgeFunctionConfiguration()
+  
+      manifest.functions.push({
+        functions: functionName,
+        name: name,
+        generator: pluginVersion
+      })
+  
+      const expected = {
+        functions: [
+          {
+            functions: functionName,
+            name: name,
+            generator: `${NEXT_PLUGIN_NAME}@${nextRuntimeVersion}`,
+          }
+      ],
+        version: 1,
+      }
+
+      const filePathToSaveTo = '.netlify/edge-functions/manifest.json'
+      await writeJSON(filePathToSaveTo, manifest)
+      const actual = await readJSON(filePathToSaveTo)
+
+    expect(actual).toEqual(expected)
+  })
+
+  it('should write the configuration for an edge function with runtime version not found', async () => {
+    mock({
+      '.netlify/edge-functions/manifest.json': JSON.stringify({
+        "functions":[],
+        "version":1
+      }),
+    })
+
+    const manifest = {
+      functions:[],
+      version:1
+      }
+  
+      const functionName = 'someFunction'
+      const name = 'someFunctionName'
+  
+      const pluginVersion = await writeEdgeFunctionConfiguration()
+  
+      manifest.functions.push({
+        functions: functionName,
+        name: name,
+        generator: pluginVersion
+      })
+  
+      const expected = {
+        functions: [
+          {
+            functions: functionName,
+            name: name,
+            generator: '@netlify/next-runtime@unknown',
+          }
+      ],
+        version: 1,
+      }
+
+      const filePathToSaveTo = '.netlify/edge-functions/manifest.json'
+      await writeJSON(filePathToSaveTo, manifest)
+      const actual = await readJSON(filePathToSaveTo)
+
+    expect(actual).toEqual(expected)
+  })
+})
+
