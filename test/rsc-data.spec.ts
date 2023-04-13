5fa14355428151f0c9748d72d96f3aea90f02bbd
Nick Taylor <nick@iamdeveloper.com>
2023-04-13 16:27:34 -0400

    fix: now data routes for dynamic routes filter even when null when creating the RSC data router (#2041)
    * fix: now data routes for dynamic routes filter even when null when creating the RSC data router
    
    * test: updated test descriptions

--- test/rsc-data.spec.ts
+++ test/rsc-data.spec.ts
@@ -8,7 +8,7 @@ const basePrerenderManifest: PrerenderManifest = {
 }
 
 describe('getRscDataRouter', () => {
-  it('should create a RSC data router when data routes are not present for routes', () => {
+  it('should filter static routes when creating the RSC data router', () => {
     const manifest: PrerenderManifest = {
       ...basePrerenderManifest,
       routes: {
@@ -21,6 +21,38 @@ describe('getRscDataRouter', () => {
           initialRevalidateSeconds: false,
           srcRoute: '/api/hello',
           dataRoute: null,
+        },
+      },
+    }
+
+    let rscDataRouter
+
+    // Normally type checking would pick this up, but because this file is copied when generating
+    // edge functions for the build, we need to make sure it's valid for builds.
+    //
+    // See https://github.com/netlify/next-runtime/issues/1940
+    expect(() => {
+      rscDataRouter = getRscDataRouter(manifest)
+    }).not.toThrow()
+
+    expect(typeof rscDataRouter).toBe('function')
+  })
+
+  it('should filter dynamic routes when creating the RSC data router', () => {
+    const manifest: PrerenderManifest = {
+      ...basePrerenderManifest,
+      dynamicRoutes: {
+        '/[slug]': {
+          routeRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)?$',
+          fallback: null,
+          dataRoute: null,
+          dataRouteRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)?$',
+        },
+        '/[slug]/[slug2]': {
+          routeRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)(?<slug2>[^\\/]+?)(?:\\/)?$',
+          fallback: null,
+          dataRoute: '/[slug]/[slug2].json.rsc',
+          dataRouteRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)(?<slug2>[^\\/]+?)(?:\\/)?$',
         },
       },
     }
