779cae66606d75006f59d51867c38ca6a8342c0c
Nick Taylor <nick@iamdeveloper.com>
2023-04-13 16:07:26 -0400

    fix: now data routes for dynamic routes filter even when null when creating the RSC data router

--- test/rsc-data.spec.ts
+++ test/rsc-data.spec.ts
@@ -37,4 +37,36 @@ describe('getRscDataRouter', () => {
 
     expect(typeof rscDataRouter).toBe('function')
   })
+
+  it('should filter dynamic routes when creating the RSC data router', () => {
+    const manifest: PrerenderManifest = {
+      ...basePrerenderManifest,
+      dynamicRoutes: {
+        '/[slug]': {
+          routeRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)?$',
+          fallback: null,
+          dataRoute: null,
+          dataRouteRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)?$',
+        },
+        '/[slug]/[slug2]': {
+          routeRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)(?<slug2>[^\\/]+?)(?:\\/)?$',
+          fallback: null,
+          dataRoute: '/[slug]/[slug2].json.rsc',
+          dataRouteRegex: '^\\/(?<slug>[^\\/]+?)(?:\\/)(?<slug2>[^\\/]+?)(?:\\/)?$',
+        },
+      },
+    }
+
+    let rscDataRouter
+
+    // Normally type checking would pick this up, but because this file is copied when generating
+    // edge functions for the build, we need to make sure it's valid for builds.
+    //
+    // See https://github.com/netlify/next-runtime/issues/1940
+    expect(() => {
+      rscDataRouter = getRscDataRouter(manifest)
+    }).not.toThrow()
+
+    expect(typeof rscDataRouter).toBe('function')
+  })
 })
