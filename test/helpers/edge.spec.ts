910be7a8927da6b928938dbf7e9375163b04f2c9
LekoArts <lekoarts@gmail.com>
2023-04-20 08:30:09 +0200

    chore: fix typo

--- test/helpers/edge.spec.ts
+++ test/helpers/edge.spec.ts
@@ -2,7 +2,7 @@ import { generateRscDataEdgeManifest } from '../../packages/runtime/src/helpers/
 import type { PrerenderManifest } from 'next/dist/build'
 
 jest.mock('../../packages/runtime/src/helpers/functionsMetaData', () => {
-  const { NEXT_PLUGIN_NAME } = require('../packages/runtime/src/constants')
+  const { NEXT_PLUGIN_NAME } = require('../../packages/runtime/src/constants')
   return {
     ...jest.requireActual('../../packages/runtime/src/helpers/functionsMetaData'),
     getPluginVersion: async () => `${NEXT_PLUGIN_NAME}@1.0.0`,
