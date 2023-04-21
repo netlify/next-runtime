0f9cf0f8e50e1d6351a2292394e415a1b55b900f
Simon Knott <info@simonknott.de>
2023-04-21 15:08:12 +0200

    feat: enable flag by default, so tests use it

--- packages/runtime/src/helpers/flags.ts
+++ packages/runtime/src/helpers/flags.ts
@@ -1,9 +1,13 @@
+import destr from 'destr'
+
 /**
- * By default, we bundle all API Routes into a single function.
- * This is fast, but can lead to large bundle sizes.
- *
  * If this flag is enabled, we generate one function per API Route.
  * We'll also use the "none" bundling strategy where we fully rely on Next.js' `.nft.json` files.
  * This should lead to smaller bundle sizes at the same speed, but is still experimental.
+ * 
+ * If disabled, we bundle all API Routes into a single function.
+ * This is fast, but can lead to large bundle sizes.
+ * 
+ * Enabled by default. Can be disabled by passing NEXT_SPLIT_API_ROUTES=false.
  */
-export const SPLIT_API_ROUTES = Boolean(process.env.NEXT_SPLIT_API_ROUTES)
+export const SPLIT_API_ROUTES = destr(process.env.NEXT_SPLIT_API_ROUTES ?? 'true')
