699ba6909a79b9833b5349ee8c090bbf8af00f7c
Simon Knott <info@simonknott.de>
2023-04-24 12:01:10 +0200

    feat: add a naïve packing algo

---
+++ test/helpers/pack.spec.ts
@@ -0,0 +1,16 @@
+import { pack } from '../../packages/runtime/src/helpers/pack'
+
+test('pack', () => {
+  expect(pack([], 0)).toEqual([])
+  expect(pack([{ value: '10', weight: 10 }], 100)).toEqual([['10']])
+  expect(
+    pack(
+      [
+        { value: '10', weight: 10 },
+        { value: '20', weight: 20 },
+        { value: '100', weight: 100 },
+      ],
+      50,
+    ),
+  ).toEqual([['100'], ['20', '10']])
+})
