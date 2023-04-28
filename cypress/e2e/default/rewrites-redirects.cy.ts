66d61c0b9cd183e3bf42ac5292277d11d8cafcd7
taty2010 <shydalys123@gmail.com>
2023-04-28 01:01:13 -0500

    chore: added failOnStatusCode: false

--- cypress/e2e/default/rewrites-redirects.cy.ts
+++ cypress/e2e/default/rewrites-redirects.cy.ts
@@ -18,10 +18,10 @@ describe('Rewrites and Redirects', () => {
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
   }
   )
-  
+
   it('redirects /getStaticProps/4 to / on LEGACY_', () => {
     Cypress.env('LEGACY_FALLBACK_FALSE', 'true')
-    cy.visit('/getStaticProps/4')
+    cy.visit('/getStaticProps/4', { failOnStatusCode: false })
     cy.url().should('eq', `${Cypress.config().baseUrl}/`)
   })
 })
