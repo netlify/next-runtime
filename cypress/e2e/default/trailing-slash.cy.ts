0325ac4f54614278484712d7d975b51066a5f597
taty2010 <shydalys123@gmail.com>
2023-05-31 16:44:29 -0500

    fix: changed testing route

--- cypress/e2e/default/trailing-slash.cy.ts
+++ cypress/e2e/default/trailing-slash.cy.ts
@@ -1,11 +1,11 @@
 describe('Trailing slash enabled', () => {
-  it('should keep the trailing slash, i.e. points /blog/erica/ to /blog/erica/', () => {
-    cy.visit('/blog/erica/')
-    cy.url().should('eq', `${Cypress.config().baseUrl}/blog/erica/`)
+  it('should keep the trailing slash, i.e. points /static/ to /static/', () => {
+    cy.visit('/static/')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/static/`)
   })
 
-  it('should put a trailing slash when there is none, i.e. points /blog/erica to /blog/erica/', () => {
-    cy.visit('/blog/erica')
-    cy.url().should('eq', `${Cypress.config().baseUrl}/blog/erica/`)
+  it('should put a trailing slash when there is none, i.e. points /static to /static/', () => {
+    cy.visit('/static')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/static/`)
   })
 })
