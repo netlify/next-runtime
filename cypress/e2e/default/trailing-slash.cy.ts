14474877650f9bf71682ab44ec116bdefce69775
taty2010 <shydalys123@gmail.com>
2023-05-31 15:33:51 -0500

    chore: updated trailing-slash

--- cypress/e2e/default/trailing-slash.cy.ts
+++ cypress/e2e/default/trailing-slash.cy.ts
@@ -1,11 +1,11 @@
 describe('Trailing slash enabled', () => {
-  it('should keep the trailing slash, i.e. points /old/ to /old/', () => {
-    cy.visit('/old/')
-    cy.url().should('eq', `${Cypress.config().baseUrl}/old/`)
+  it('should keep the trailing slash, i.e. points /blog/erica/ to /blog/erica/', () => {
+    cy.visit('/blog/erica/')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/blog/erica/`)
   })
 
-  it('should put a trailing slash when there is none, i.e. points /old to /old/', () => {
-    cy.visit('/old')
-    cy.url().should('eq', `${Cypress.config().baseUrl}/old/`)
+  it('should put a trailing slash when there is none, i.e. points /blog/erica to /blog/erica/', () => {
+    cy.visit('/blog/erica')
+    cy.url().should('eq', `${Cypress.config().baseUrl}/blog/erica/`)
   })
 })
