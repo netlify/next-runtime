6817cc93b8147edc95558ad8ea0f9fdb058832e1
Rob Stanford <me@robstanford.com>
2023-06-16 16:04:58 +0100

    fix: new ISR cache handling to resolve regression in 13.4 (#2165)
    Co-authored-by: Nick Taylor <nick@iamdeveloper.com>

--- packages/runtime/src/index.ts
+++ packages/runtime/src/index.ts
@@ -17,7 +17,7 @@ import {
 } from './helpers/config'
 import { onPreDev } from './helpers/dev'
 import { writeEdgeFunctions, loadMiddlewareManifest, cleanupEdgeFunctions } from './helpers/edge'
-import { moveStaticPages, movePublicFiles, patchNextFiles, removeMetadataFiles } from './helpers/files'
+import { moveStaticPages, movePublicFiles, removeMetadataFiles } from './helpers/files'
 import { splitApiRoutes } from './helpers/flags'
 import {
   generateFunctions,
@@ -183,8 +183,6 @@ const plugin: NetlifyPlugin = {
     })
 
     await movePublicFiles({ appDir, outdir, publish, basePath })
-
-    await patchNextFiles(appDir)
 
     if (!destr(process.env.SERVE_STATIC_FILES_FROM_ORIGIN)) {
       await moveStaticPages({ target, netlifyConfig, i18n, basePath })
