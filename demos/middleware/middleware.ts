5369c26778445fd35b856af076217c58bad9752e
taty2010 <shydalys123@gmail.com>
2023-06-26 16:51:56 -0500

    fix: adding headers fix

--- demos/middleware/middleware.ts
+++ demos/middleware/middleware.ts
@@ -38,7 +38,13 @@ export async function middleware(req: NextRequest) {
 
   if (pathname.startsWith('/request-rewrite')) {
     // request.rewrite() should return the MiddlewareResponse object instead of the Response object.
-    const res = await request.rewrite('/static-rewrite')
+    const res = await request.rewrite('/static-rewrite',
+    {
+      headers: {
+        'x-rewrite-test': 'hello',
+        'x-rewrite-test-2': 'hello-2'
+      }
+    })
     const message = `This was static (& escaping test &amp;) but has been transformed in ${req.geo?.city}`
 
     // Transform the response HTML and props
