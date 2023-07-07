48b66a674fa32349bc975e1486a32a6d0d7100ec
Marc Littlemore <marc.littlemore@netlify.com>
2023-07-07 14:58:50 +0100

    chore: add comment

--- packages/runtime/src/helpers/logger/index.ts
+++ packages/runtime/src/helpers/logger/index.ts
@@ -1,5 +1,7 @@
-// Pino only supports JSON output
+// Pino only supports JSON output but this uses a pretty formatter
+// It will still give timestamps and log levels though
 // export { logger as default } from './pino'
 
 // Winston supports JSON by default but this is using a custom formatter
+// It only gives the message and not the timestamp or log level
 export { logger as default } from './winston'
