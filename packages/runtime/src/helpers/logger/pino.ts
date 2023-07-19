6af26516c50de331f6624c7d2c02f8a831e4ead5
Marc Littlemore <marc.littlemore@netlify.com>
2023-07-19 12:01:39 +0100

    chore: remove pino

--- packages/runtime/src/helpers/logger/pino.ts
+++
@@ -1,5 +0,0 @@
-import pino from 'pino'
-// Should only be used in development but buildbot expects STDOUT
-import pretty from 'pino-pretty'
-
-export const logger = pino(pretty())
--- packages/runtime/src/helpers/logger/winston.ts
+++ packages/runtime/src/helpers/logger/winston.ts
@@ -1,7 +1,10 @@
 import winston from 'winston'
 
+// Define log level from environment variable so we can change it if needed
+const logLevel = process.env.LOG_LEVEL ?? 'info';
+
 export const logger = winston.createLogger({
-  transports: [new winston.transports.Console()],
+  transports: [new winston.transports.Console({ level: logLevel })],
   // Ignore the log level and just show the message
   // Use winston.format.simple() to include the log level
   format: winston.format.printf(({ message }) => message),
