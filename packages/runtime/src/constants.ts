048a0c22f831466aef1960bc20cf2e34b81e2514
Nick Taylor <nick@nickyt.co>
2023-09-06 17:41:35 -0400

    chore: reverted some refactor not related to the PR

--- packages/runtime/src/constants.ts
+++ packages/runtime/src/constants.ts
@@ -10,36 +10,30 @@ export const HANDLER_FUNCTION_TITLE = 'Next.js SSR handler'
 export const ODB_FUNCTION_TITLE = 'Next.js ISR handler'
 export const API_FUNCTION_TITLE = 'Next.js API handler'
 export const IMAGE_FUNCTION_TITLE = 'next/image handler'
-
-const hiddenPaths = () => {
-  const paths = destr(process.env.NEXT_KEEP_METADATA_FILES)
-    ? []
-    : [
-        '/cache',
-        '/server',
-        '/serverless',
-        '/trace',
-        '/traces',
-        '/routes-manifest.json',
-        '/build-manifest.json',
-        '/prerender-manifest.json',
-        '/react-loadable-manifest.json',
-        '/BUILD_ID',
-        '/app-build-manifest.json',
-        '/app-path-routes-manifest.json',
-        '/export-marker.json',
-        '/images-manifest.json',
-        '/next-server.js.nft.json',
-        '/package.json',
-        '/prerender-manifest.js',
-        '/required-server-files.json',
-        '/static-manifest.json',
-      ]
-  return paths
-}
-
 // These are paths in .next that shouldn't be publicly accessible
-export const HIDDEN_PATHS = hiddenPaths()
+export const HIDDEN_PATHS = destr(process.env.NEXT_KEEP_METADATA_FILES)
+  ? []
+  : [
+      '/cache',
+      '/server',
+      '/serverless',
+      '/trace',
+      '/traces',
+      '/routes-manifest.json',
+      '/build-manifest.json',
+      '/prerender-manifest.json',
+      '/react-loadable-manifest.json',
+      '/BUILD_ID',
+      '/app-build-manifest.json',
+      '/app-path-routes-manifest.json',
+      '/export-marker.json',
+      '/images-manifest.json',
+      '/next-server.js.nft.json',
+      '/package.json',
+      '/prerender-manifest.js',
+      '/required-server-files.json',
+      '/static-manifest.json',
+    ]
 
 export const ODB_FUNCTION_PATH = `/.netlify/builders/${ODB_FUNCTION_NAME}`
 export const HANDLER_FUNCTION_PATH = `/.netlify/functions/${HANDLER_FUNCTION_NAME}`
