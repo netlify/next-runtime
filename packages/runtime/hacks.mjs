a118b9ec50c760f99c3d42377748df7e8cadeda3
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-09-26 12:50:38 +0200

    test: will this be set?6

---
+++ packages/runtime/hacks.mjs
@@ -0,0 +1,10 @@
+import fs from 'fs-extra'
+
+const blobContent = await fs.readFile('src/blob.js', 'utf8')
+const blobContentWithHacks = blobContent.replace(
+  `const encodedKey = encodeURIComponent(key);`,
+  `const encodedKey = encodeURIComponent(key);\n    console.log({ key, encodedKey });`,
+)
+
+await fs.writeFile('src/blob.js', blobContentWithHacks)
+console.log(`added logging`)
