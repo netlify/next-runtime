560639c92e99aa7ae215b831c9865a9d4f324278
Rob Stanford <me@robstanford.com>
2023-09-27 14:44:13 +0100

    chore: remove unnecessary comments

--- src/helpers/files.ts
+++ src/helpers/files.ts
@@ -9,11 +9,8 @@ import { NETLIFY_PUBLISH_DIR } from './constants.js'
  * @param publishDir The publish directory
  */
 export const publishStaticAssets = (publishDir: string) => {
-  // copy user's public folder to the new publish directory
   if (existsSync('public')) {
     copySync('public', NETLIFY_PUBLISH_DIR, { overwrite: true })
   }
-
-  // copy the Next.js static assets to the new publish directory
   copySync(`${publishDir}/static/`, `${NETLIFY_PUBLISH_DIR}/_next/static`, { overwrite: true })
 }
