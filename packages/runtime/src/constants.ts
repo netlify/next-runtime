dec788896785a70357d919904e7ceac876f8d296
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-09-28 14:49:37 +0200

    fix: remove blobs-manifest before publishing to cdn

--- packages/runtime/src/constants.ts
+++ packages/runtime/src/constants.ts
@@ -33,7 +33,7 @@ export const HIDDEN_PATHS = destr(process.env.NEXT_KEEP_METADATA_FILES)
       '/prerender-manifest.js',
       '/required-server-files.json',
       '/static-manifest.json',
-      '/blob-manifest.json',
+      '/blobs-manifest.json',
     ]
 
 export const ODB_FUNCTION_PATH = `/.netlify/builders/${ODB_FUNCTION_NAME}`
