24956d1bf2aae331ab53327d67aab8d4ec79be0b
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-09-29 12:42:34 +0200

    test: remove logs from fetch polyfill that were breaking ntl deploy output parsing

--- test/e2e/next-test-lib/fetch-polyfill.js
+++ test/e2e/next-test-lib/fetch-polyfill.js
@@ -1,12 +1,8 @@
 if (!globalThis.fetch) {
-  console.log(`injecting global fetch polyfill`)
-
   const fetch = require('node-fetch')
 
   globalThis.fetch = fetch
   globalThis.Headers = fetch.Headers
   globalThis.Request = fetch.Request
   globalThis.Response = fetch.Response
-
-  console.log(`injected global fetch polyfill`, globalThis.fetch)
 }
