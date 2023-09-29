3275332d41e0f99f361680c0b287a21367cf77fc
Michal Piechowiak <misiek.piechowiak@gmail.com>
2023-09-29 12:25:34 +0200

    test: inject global fetch polyfill2

--- test/e2e/next-test-lib/fetch-polyfill.js
+++ test/e2e/next-test-lib/fetch-polyfill.js
@@ -1,5 +1,12 @@
 if (!globalThis.fetch) {
   console.log(`injecting global fetch polyfill`)
-  globalThis.fetch = require('node-fetch')
+
+  const fetch = require('node-fetch')
+
+  globalThis.fetch = fetch
+  globalThis.Headers = fetch.Headers
+  globalThis.Request = fetch.Request
+  globalThis.Response = fetch.Response
+
   console.log(`injected global fetch polyfill`, globalThis.fetch)
 }
