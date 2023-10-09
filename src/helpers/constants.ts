a977043c864020ca8df32ff925409a7f20733851
Rob Stanford <me@robstanford.com>
2023-10-09 21:24:56 +0100

    fix: update server handler name

--- src/helpers/constants.ts
+++ src/helpers/constants.ts
@@ -7,6 +7,6 @@ export const NETLIFY_PUBLISH_DIR = '.netlify/publish'
 export const FUNCTIONS_INTERNAL_DIR = '.netlify/functions-internal'
 export const FUNCTIONS_URL = '/.netlify/functions'
 
-export const SERVER_HANDLER_NAME = '___netlify-handler'
+export const SERVER_HANDLER_NAME = '___netlify-server-handler'
 export const SERVER_HANDLER_DIR = `${FUNCTIONS_INTERNAL_DIR}/${SERVER_HANDLER_NAME}`
 export const SERVER_HANDLER_URL = `${FUNCTIONS_URL}/${SERVER_HANDLER_NAME}`
