c0cc952e731020caeff17339c1d35160547b5bfb
Rob Stanford <me@robstanford.com>
2023-10-13 17:28:46 +0100

    feat: revert to nft due to output issues with bundling

--- src/helpers/functions.ts
+++ src/helpers/functions.ts
@@ -1,31 +1,36 @@
 import { writeFileSync } from 'fs'
 
-import { NetlifyConfig } from '@netlify/build'
-import { build } from 'esbuild'
+import { nodeFileTrace } from '@vercel/nft'
 import { copySync, emptyDirSync, readJsonSync, writeJSONSync } from 'fs-extra/esm'
 
-import {
-  NETLIFY_TEMP_DIR,
-  SERVER_HANDLER_DIR,
-  SERVER_HANDLER_NAME,
-  SERVER_HANDLER_URL,
-  PLUGIN_DIR,
-} from './constants.js'
+import { NEXT_BUILD_DIR, SERVER_HANDLER_DIR, SERVER_HANDLER_NAME, PLUGIN_DIR } from './constants.js'
+import { write } from 'fs-extra'
+
+const pkg = readJsonSync(`${PLUGIN_DIR}/package.json`)
 
 /**
  * Create a Netlify function to run the Next.js server
  * @param publishDir The publish directory
  * @param config Netlify config
  */
-export const createServerHandler = async (publishDir: string, config: NetlifyConfig) => {
+export const createServerHandler = async () => {
   // clear the server handler directory
   emptyDirSync(SERVER_HANDLER_DIR)
 
-  // copy the next.js standalone build output to the server handler directory
-  copySync(`${publishDir}/standalone/.next`, `${SERVER_HANDLER_DIR}/.next`)
-  copySync(`${publishDir}/standalone/node_modules`, `${SERVER_HANDLER_DIR}/node_modules`)
+  // trace the server handler dependencies
+  const { fileList } = await nodeFileTrace(
+    [`${PLUGIN_DIR}/dist/templates/server-handler.js`, `${PLUGIN_DIR}/dist/templates/cache-handler.cjs`],
+    { base: PLUGIN_DIR, ignore: ['package.json', 'node_modules/next/**'] },
+  )
 
-  const pkg = readJsonSync(`${PLUGIN_DIR}/package.json`)
+  // copy the handler dependencies
+  fileList.forEach((path) => {
+    copySync(`${PLUGIN_DIR}/${path}`, `${SERVER_HANDLER_DIR}/${path}`)
+  })
+
+  // copy the next.js standalone build output to the server handler directory
+  copySync(`${NEXT_BUILD_DIR}/standalone/.next`, `${SERVER_HANDLER_DIR}/.next`)
+  copySync(`${NEXT_BUILD_DIR}/standalone/node_modules`, `${SERVER_HANDLER_DIR}/node_modules`)
 
   // create the server handler metadata file
   writeJSONSync(`${SERVER_HANDLER_DIR}/${SERVER_HANDLER_NAME}.json`, {
@@ -33,54 +38,16 @@ export const createServerHandler = async (publishDir: string, config: NetlifyCon
       name: 'Next.js Server Handler',
       generator: `${pkg.name}@${pkg.version}`,
       nodeBundler: 'none',
-      // TODO: remove the final include when Netlify Functions v2 fixes the default exports bug
-      includedFiles: ['.next/**', 'node_modules/**', `${SERVER_HANDLER_NAME}*`],
+      includedFiles: [`${SERVER_HANDLER_NAME}*`, 'package.json', 'dist/**', '.next/**', 'node_modules/**'],
       includedFilesBasePath: SERVER_HANDLER_DIR,
     },
     version: 1,
   })
 
-  // bundle the cache handler
-  await build({
-    entryPoints: [`${PLUGIN_DIR}/dist/templates/cache-handler.js`],
-    bundle: true,
-    platform: 'node',
-    target: ['node18'],
-    format: 'cjs',
-    outfile: `${SERVER_HANDLER_DIR}/${SERVER_HANDLER_NAME}-cache.js`,
-  })
+  writeFileSync(`${SERVER_HANDLER_DIR}/package.json`, JSON.stringify({ type: 'module' }))
 
-  // bundle the server handler
-  await build({
-    entryPoints: [`${PLUGIN_DIR}/dist/templates/server-handler.js`],
-    bundle: true,
-    platform: 'node',
-    target: ['node18'],
-    format: 'esm',
-    external: ['next'],
-    outfile: `${SERVER_HANDLER_DIR}/${SERVER_HANDLER_NAME}-actual.mjs`,
-  })
-
-  // TODO: remove when Netlify Functions v2 fixes the default exports bug
-  // https://github.com/netlify/pod-dev-foundations/issues/599
   writeFileSync(
-    `${SERVER_HANDLER_DIR}/${SERVER_HANDLER_NAME}.mjs`,
-    `import handler from './${SERVER_HANDLER_NAME}-actual.mjs';export default (request) => handler(request);`,
+    `${SERVER_HANDLER_DIR}/${SERVER_HANDLER_NAME}.js`,
+    `import handler from './dist/templates/server-handler.js';export default (request) => handler;export const config = { path: '/*' }`,
   )
-
-  // TODO: remove this when we can use inline config
-  // https://github.com/netlify/next-runtime-minimal/issues/13
-  config.redirects ||= []
-  config.redirects.push({ from: `/*`, to: SERVER_HANDLER_URL, status: 200 })
-}
-
-export const createCacheHandler = async () => {
-  await build({
-    entryPoints: [`${PLUGIN_DIR}/dist/templates/cache-handler.js`],
-    bundle: true,
-    platform: 'node',
-    target: ['node18'],
-    format: 'cjs',
-    outfile: `${NETLIFY_TEMP_DIR}/cache-handler.js`,
-  })
 }
--- src/index.ts
+++ src/index.ts
@@ -1,22 +1,17 @@
 import type { NetlifyPluginOptions } from '@netlify/build'
 
-import { modifyNetlifyConfig, modifyNextConfig, revertNextConfig } from './helpers/config.js'
-import { publishStaticAssets } from './helpers/files.js'
-import { createServerHandler, createCacheHandler } from './helpers/functions.js'
+import { setBuildtimeConfig } from './helpers/config.js'
+import { moveStaticAssets, stashBuildOutput } from './helpers/files.js'
+import { createServerHandler } from './helpers/functions.js'
 
 type NetlifyPluginOptionsWithFlags = NetlifyPluginOptions & { featureFlags?: Record<string, unknown> }
 
-export const onPreBuild = async () => {
-  await createCacheHandler()
-  modifyNextConfig()
+export const onPreBuild = () => {
+  setBuildtimeConfig()
 }
 
-export const onBuild = async ({ constants, netlifyConfig }: NetlifyPluginOptionsWithFlags) => {
-  publishStaticAssets(constants.PUBLISH_DIR)
-  await createServerHandler(constants.PUBLISH_DIR, netlifyConfig)
-  modifyNetlifyConfig(netlifyConfig)
-}
-
-export const onEnd = () => {
-  revertNextConfig()
+export const onBuild = async ({ constants }: NetlifyPluginOptionsWithFlags) => {
+  stashBuildOutput(constants)
+  moveStaticAssets(constants)
+  await createServerHandler()
 }
