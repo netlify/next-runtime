59249ac834fec3251e9c0d4f1ccc24485727fea2
Tatyana <shydalys123@gmail.com>
2023-10-18 15:38:43 -0500

    feat: cleanup + officially adding files to blob

--- src/helpers/files.ts
+++ src/helpers/files.ts
@@ -1,17 +1,19 @@
 /* eslint-disable import/no-extraneous-dependencies */
-import { existsSync, readFileSync} from 'node:fs'
-import { resolve, join } from 'node:path'
+import { existsSync } from 'node:fs'
+import { resolve } from 'node:path'
 import { cpus } from 'os'
 
 import { NetlifyPluginConstants } from '@netlify/build'
 import type { NetlifyConfig } from '@netlify/build'
 import pkg from 'fs-extra'
-import { copy, move, remove, readJSON, writeJson } from 'fs-extra/esm'
+import { copy, move, remove, readJSON } from 'fs-extra/esm'
 import { globby } from 'globby'
+import { outdent } from 'outdent'
 import pLimit from 'p-limit'
 
-import { getNormalizedBlobKey } from './blobs.cjs'
-import { BUILD_DIR, SERVER_APP_DIR, STANDALONE_APP_DIR } from './constants.js'
+import { netliBlob, getNormalizedBlobKey } from './blobs.cjs'
+import { BUILD_DIR, STANDALONE_BUILD_DIR } from './constants.js'
+
 
 // readfile not available in esm version of fs-extra
 const { readFile } = pkg
@@ -22,9 +24,9 @@ export const stashBuildOutput = async ({ PUBLISH_DIR }: NetlifyPluginConstants) 
   await move(PUBLISH_DIR, BUILD_DIR, { overwrite: true })
 
   // remove prerendered content from the standalone build (it's also in the main build dir)
-  const prerenderedContent = await getPrerenderedContent(STANDALONE_APP_DIR)
+  const prerenderedContent = await getPrerenderedContent(STANDALONE_BUILD_DIR)
   await Promise.all(
-    prerenderedContent.map((file: string) => remove(`${STANDALONE_APP_DIR}/${file}`))).catch((error) => console.error(error))
+    prerenderedContent.map((file: string) => remove(`${STANDALONE_BUILD_DIR}/${file}`))).catch((error) => console.error(error))
 
 }
 
@@ -33,62 +35,55 @@ export const stashBuildOutput = async ({ PUBLISH_DIR }: NetlifyPluginConstants) 
  */
 const getPrerenderedContent = async (cwd: string): Promise<string[]> => {
   // TODO: test this
-  return await globby(['**/*.+(html|json|rsc|body|meta)', '!**/*.js.nft.{html,json}'], { cwd, extglob: true })
+  return await globby([
+    `cache/fetch-cache/*`,
+    `server/+(app|pages)/**/*.+(html|json|rsc|body|meta)`,
+    `!server/**/*.js.nft.{html,json}`,
+  ], { cwd, extglob: true })
 }
 
 /**
  * Upload prerendered content from the main build dir to the blob store
  */
-// Adding intiail logic to upload prerendered content to blob storage still a WIP
-export const storePrerenderedContent = async (netlifyConfig, basePath, target, netliBlob) => {
-  const buildId = readFileSync(join(BUILD_DIR, 'BUILD_ID'), 'utf8').trim()
-  const outputDir = join(netlifyConfig.build.publish, target === 'serverless' ? 'serverless' : 'server')
-  const dataDir = join('_next', 'data', buildId)
+export const storePrerenderedContent = async ({ NETLIFY_API_TOKEN, SITE_ID }:
+  { 
+    NETLIFY_API_TOKEN: string, SITE_ID: string
+  }) => {
+    const deployID = `${process.env.DEPLOY_ID}`
+    const blob = netliBlob(NETLIFY_API_TOKEN, deployID, SITE_ID)
+    // todo: Check out setFiles within Blobs.js to see how to upload files to blob storage
+    const limit = pLimit(Math.max(2, cpus().length))
 
-  let blobCount = 0
-  const blobsManifest = new Set<string>()
-  const limit = pLimit(Math.max(2, cpus().length))
-  const prerenderedContent = await getPrerenderedContent(SERVER_APP_DIR)
-
-  const getSourceAndTargetPath = (file: string): { source: string } => {
-    const source = join(SERVER_APP_DIR, file)
-    // Strip the initial 'app' or 'pages' directory from the output path
-    // const pathname = file.split('/').slice(1).join('/')
-    // // .rsc data files go next to the html file
-    // const isData = file.endsWith('.json')
-    // const targetFile = isData ? join(dataDir, pathname) : pathname
-    // const targetPath = basePath ? join(basePath, targetFile) : targetFile
-
-    return {
-      // targetPath,
-      source,
+    const uploadFilesToBlob = async (key: string, file: string) => {
+      try{
+        console.log(
+          outdent`
+            Uploading Files to Blob Storage...
+          `)
+        const content = await readFile(file, 'utf8')
+        await blob.set(getNormalizedBlobKey(key), content)
+      }catch(error){
+        console.error(error)
+      }
     }
-  }
-  const uploadFilesToBlob = async (file: string) => {
-    const { source } = getSourceAndTargetPath(file)
-    blobsManifest.add(file)
-    
-    const content = await readFile(source, 'utf8')
 
-    blobCount += 1
+    // todo: test it on demo with pages dir
+    const prerenderedContent = await getPrerenderedContent(BUILD_DIR)
 
-    // await netliBlob.set(getNormalizedBlobKey(file), content)
-    await remove(source)
-  }
-
-  // eslint-disable-next-line array-callback-return
-  prerenderedContent.map((rawPath) => {
-    const pagePath = rawPath.split('/').slice(1).join('/')
-    // Checking if Blob Storage is enabled to upload prerendered content and delete it from the build directory
-    if( netliBlob ){
-      return limit(uploadFilesToBlob, rawPath)
-    }
-    
-  })
-  // TODO: implement
-  await writeJson(join(netlifyConfig.build.publish, 'blobs-manifest.json'), [...blobsManifest])
+    prerenderedContent.map((rawPath) => {
+      const cacheFile = rawPath.startsWith('cache')
+      // Removing app, page, and cache/fetch-cache from file path
+      const pathKey = removeFileDir(rawPath, (cacheFile ? 2 : 1))
+      // Checking for blob access before uploading
+      if( blob ){
+        return limit(uploadFilesToBlob, pathKey, rawPath)
+      }
+    })
 }
 
+const removeFileDir = (file: string, num: number) => {
+  return file.split('/').slice(num).join('/')
+}
 
 const maybeLoadJson = <T>(path: string): Promise<T> | null => existsSync(path) ? readJSON(path) : null
 
