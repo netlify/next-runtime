d53739fde52dea09b5a287d97427733ef3cc8490
Lukas Holzer <lukas.holzer@typeflow.cc>
2023-12-06 14:45:55 +0100

    chore: make test setup log errors (#99)

--- tests/fixtures/simple-next-app-pnpm/next.config.js
+++ tests/fixtures/simple-next-app-pnpm/next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  swcMinify: true,
   output: 'standalone',
   eslint: {
     ignoreDuringBuilds: true,
--- tests/prepare.mjs
+++ tests/prepare.mjs
@@ -1,3 +1,4 @@
+// @ts-check
 // this installs and builds all the fixtures
 // Needed to run before executing the integration tests
 import { execaCommand } from 'execa'
@@ -5,23 +6,45 @@ import { existsSync, readdirSync } from 'node:fs'
 import { rm } from 'node:fs/promises'
 import { join } from 'node:path'
 import { fileURLToPath } from 'node:url'
+import { cpus } from 'node:os'
+import pLimit from 'p-limit'
+import { Transform } from 'node:stream'
 
 const fixturesDir = fileURLToPath(new URL(`./fixtures`, import.meta.url))
 
-console.log('Preparing test fixtures:')
+const limit = pLimit(Math.max(2, cpus().length))
 await Promise.all(
-  readdirSync(fixturesDir).map(async (fixture) => {
-    console.log(`◆ Preparing fixture: ${fixture}`)
-    await rm(join(fixturesDir, fixture, '.next'), { recursive: true, force: true })
-    const cwd = join(fixturesDir, fixture)
+  readdirSync(fixturesDir).map((fixture) =>
+    limit(async () => {
+      console.log(`[${fixture}] Preparing fixture`)
+      await rm(join(fixturesDir, fixture, '.next'), { recursive: true, force: true })
+      const cwd = join(fixturesDir, fixture)
 
-    // npm is the default
-    let cmd = `npm install --no-audit --progress=false --prefer-offline`
+      // npm is the default
+      let cmd = `npm install --no-audit --progress=false --prefer-offline`
 
-    if (existsSync(join(cwd, 'pnpm-lock.yaml'))) {
-      cmd = `pnpm install --reporter=silent`
-    }
+      if (existsSync(join(cwd, 'pnpm-lock.yaml'))) {
+        cmd = `pnpm install --reporter=silent`
+      }
 
-    return execaCommand(cmd, { cwd })
-  }),
+      const addPrefix = new Transform({
+        transform(chunk, encoding, callback) {
+          this.push(chunk.toString().replace(/\n/gm, `\n[${fixture}] `))
+          callback()
+        },
+      })
+
+      const output = execaCommand(cmd, {
+        cwd,
+        stdio: 'pipe',
+        env: { ...process.env, FORCE_COLOR: '1' },
+      })
+      if (process.env.DEBUG) {
+        output.stdout?.pipe(addPrefix).pipe(process.stdout)
+      }
+      output.stderr?.pipe(addPrefix).pipe(process.stderr)
+
+      return output
+    }),
+  ),
 )
