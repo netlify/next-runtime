da7aa22fb0ceea68eaf39ecb49997077d81a36c5
Matt Kane <m@mk.gg>
2024-02-01 15:09:04 +0000

    fix: correctly handle middleware header mutations (#225)
    * fix: correctly handle middleware header mutations
    
    * Add comment and remove log

--- edge-runtime/lib/headers.ts
+++ edge-runtime/lib/headers.ts
@@ -12,20 +12,34 @@ export const InternalHeaders = {
 // * https://github.com/vercel/next.js/blob/43c9d8940dc42337dd2f7d66aa90e6abf952278e/packages/next/server/web/spec-extension/response.ts#L10-L27
 export function updateModifiedHeaders(requestHeaders: Headers, responseHeaders: Headers) {
   const overriddenHeaders = responseHeaders.get('x-middleware-override-headers')
-
   if (!overriddenHeaders) {
     return
   }
 
-  const headersToUpdate = overriddenHeaders.split(',').map((header) => header.trim())
+  const headersToUpdate = new Set(overriddenHeaders.split(',').map((header) => header.trim()))
+
+  // We can't iterate this directly, because we modify the headers in the loop.
+  // This was causing values to be skipped. By spreading them first we avoid that.
+  for (const key of [...requestHeaders.keys()]) {
+    if (!headersToUpdate.has(key)) {
+      requestHeaders.delete(key)
+    }
+  }
 
   for (const header of headersToUpdate) {
     const oldHeaderKey = 'x-middleware-request-' + header
     const headerValue = responseHeaders.get(oldHeaderKey) || ''
 
-    requestHeaders.set(header, headerValue)
+    const oldValue = requestHeaders.get(header) || ''
+
+    if (oldValue !== headerValue) {
+      if (headerValue) {
+        requestHeaders.set(header, headerValue)
+      } else {
+        requestHeaders.delete(header)
+      }
+    }
     responseHeaders.delete(oldHeaderKey)
   }
-
   responseHeaders.delete('x-middleware-override-headers')
 }
