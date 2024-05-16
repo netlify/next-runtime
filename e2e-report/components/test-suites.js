1b5aa8384565d6034a467a1d4a0141cb8b47ade2
Tatyana <43764894+taty2010@users.noreply.github.com>
2024-05-16 09:39:14 -0500

    chore: e2e tests results page (#422)
    * site setup
    
    * add timestamp to json data
    
    * download data to e2e site
    
    * remove site from report
    
    * chore: format with prettier
    
    * update path
    
    * deploy preview on test run
    
    * updaate chart to use group data
    
    * put % and # in right place
    
    * smol updates
    
    * remove slack notification
    
    * update svgs and styles
    
    * add skipped test suites to drpdwns
    
    * remove skipped from main results
    
    * remove deploy step
    
    * remove deployInfo2slack.ts
    
    * rename svg file to make casing consistent
    
    * configure node version in package.json and remove eslint-disable for unsupported-featrures
    
    * Apply suggestions from code review
    
    Co-authored-by: Philippe Serhal <philippe.serhal@netlify.com>
    
    * remove unused classnames
    
    * get result of command and not command
    
    * provide token
    
    * specify repo
    
    * checkout repo and provide ntl auth token
    
    * install site deps
    
    * install deps in site directory and not repo root
    
    * remove sample test-results.json before downloading fresh one
    
    * checkout early
    
    * define test groups in single location
    
    * destroy the chart in useEffect cleanup instead of trying to find previous one and destroying it early in useEffect
    
    * don't need env per step
    
    * fix Inter font
    
    * use standard bold weight to avoid fake bold
    
    * don't use root repo eslint config for report site
    
    * bump site deps
    
    * remove no longer unit script
    
    ---------
    
    Co-authored-by: taty2010 <taty2010@users.noreply.github.com>
    Co-authored-by: Michal Piechowiak <misiek.piechowiak@gmail.com>
    Co-authored-by: Philippe Serhal <philippe.serhal@netlify.com>

---
+++ e2e-report/components/test-suites.js
@@ -0,0 +1,50 @@
+import Table from './table.js'
+
+export default function TestSuites({ suite, slider, handleSelect, arrows }) {
+  const { name, file, passed, failed, skipped, testCases } = suite
+  // TODO: There are some suites coming through that have no status and some repeats
+  if (skipped === true || (passed === 0 && failed === 0 && skipped === 0)) {
+    return
+  }
+
+  return (
+    <>
+      <div onClick={() => handleSelect(name)} className="card">
+        <Table
+          th={['Test suite:', '# of tests:', 'Passing:']}
+          name={name}
+          passed={passed}
+          total={passed + failed}
+        />
+        {arrows(slider[name])}
+      </div>
+      <ul className={`testCases ${slider[name] ? 'open' : 'close'}`}>
+        {testCases?.map((testCase, index) => {
+          const { status, link, reason } = testCase
+
+          if (status === 'skipped') {
+            return
+          }
+
+          return (
+            <li key={`${name}${index}`}>
+              <a target="_blank" href={`https://github.com/vercel/next.js/tree/canary/${file}`}>
+                {testCase.name}
+              </a>
+              <span className="status" data-status={status}></span>
+              {reason && <p>Reason: {reason}</p>}
+              {link && (
+                <button className="github">
+                  <a href={link} target="_blank">
+                    {' '}
+                    Github Issue
+                  </a>
+                </button>
+              )}
+            </li>
+          )
+        })}
+      </ul>
+    </>
+  )
+}
---
+++ e2e-report/next.config.mjs
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ['@svgr/webpack'],
+    })
+    return config
+  },
+}
+
+export default nextConfig
