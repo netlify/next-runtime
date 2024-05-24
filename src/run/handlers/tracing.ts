b45be14d0217dcf947375572b93431c3e5f98145
Michal Piechowiak <misiek.piechowiak@gmail.com>
2024-05-24 12:10:07 +0200

    fix lint

--- src/run/handlers/tracing.ts
+++ src/run/handlers/tracing.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line max-classes-per-file
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http'
 import { Resource } from '@opentelemetry/resources'
 import { NodeSDK } from '@opentelemetry/sdk-node'
