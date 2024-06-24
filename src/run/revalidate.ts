b475810e3d8498ef4fdbcf5fabf48250c78aabd2
Michal Piechowiak <misiek.piechowiak@gmail.com>
2024-06-24 11:01:24 +0200

    fix: remove 'any' types from next's response proxy

--- src/run/revalidate.ts
+++ src/run/revalidate.ts
@@ -1,20 +1,29 @@
 import type { ServerResponse } from 'node:http'
 import { isPromise } from 'node:util/types'
 
+import type { NextApiResponse } from 'next'
+
 import type { RequestContext } from './handlers/request-context.cjs'
+
+type ResRevalidateMethod = NextApiResponse['revalidate']
+
+function isRevalidateMethod(
+  key: string,
+  nextResponseField: unknown,
+): nextResponseField is ResRevalidateMethod {
+  return key === 'revalidate' && typeof nextResponseField === 'function'
+}
 
 // Needing to proxy the response object to intercept the revalidate call for on-demand revalidation on page routes
 export const nextResponseProxy = (res: ServerResponse, requestContext: RequestContext) => {
   return new Proxy(res, {
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    get(target: any[string], key: string) {
-      const originalValue = target[key]
-      if (key === 'revalidate') {
-        // eslint-disable-next-line @typescript-eslint/no-explicit-any
-        return async function newRevalidate(...args: any[]) {
+    get(target: ServerResponse, key: string) {
+      const originalValue = Reflect.get(target, key)
+      if (isRevalidateMethod(key, originalValue)) {
+        return function newRevalidate(...args: Parameters<ResRevalidateMethod>) {
           requestContext.didPagesRouterOnDemandRevalidate = true
 
-          const result = originalValue?.apply(target, args)
+          const result = originalValue.apply(target, args)
           if (result && isPromise(result)) {
             requestContext.trackBackgroundWork(result)
           }
