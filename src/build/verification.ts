b30752b93f20c5943c8e2b86dc1860b557e4dea6
Rob Stanford <me@robstanford.com>
2024-07-09 19:46:27 +0100

    feat: check env var before verifying netlify forms

--- src/build/verification.ts
+++ src/build/verification.ts
@@ -112,7 +112,12 @@ export async function verifyNetlifyFormsWorkaround(ctx: PluginContext) {
 }
 
 export function verifyNetlifyForms(ctx: PluginContext, html: string) {
-  if (!verifications.has('netlifyFormsWorkaround') && formDetectionRegex.test(html)) {
+  if (
+    process.env.NETLIFY_NEXT_VERIFY_FORMS !== '0' &&
+    process.env.NETLIFY_NEXT_VERIFY_FORMS?.toUpperCase() !== 'FALSE' &&
+    !verifications.has('netlifyFormsWorkaround') &&
+    formDetectionRegex.test(html)
+  ) {
     ctx.failBuild(
       '@netlify/plugin-nextjs@5 requires migration steps to support Netlify Forms. Refer to https://ntl.fyi/next-runtime-forms-migration for migration example.',
     )
