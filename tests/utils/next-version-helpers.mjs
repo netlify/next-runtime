d41873beb3fe295022e023481f34d3b725d27cd2
pieh <misiek.piechowiak@gmail.com>
2024-08-22 11:56:48 +0200

    update react version needed by next@canary

--- tests/utils/next-version-helpers.mjs
+++ tests/utils/next-version-helpers.mjs
@@ -9,7 +9,7 @@ const FUTURE_NEXT_PATCH_VERSION = '14.999.0'
 
 const NEXT_VERSION_REQUIRES_REACT_19 = '14.3.0-canary.45'
 // TODO: Update this when React 19 is released
-const REACT_19_VERSION = '19.0.0-rc.0'
+const REACT_19_VERSION = '19.0.0-rc-1eaccd82-20240816'
 const REACT_18_VERSION = '18.2.0'
 
 /**
