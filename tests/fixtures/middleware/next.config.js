ec5e1f5447c7a2e6c5044630baa84d762645942e
pieh <misiek.piechowiak@gmail.com>
2024-08-26 12:09:47 +0200

    test: make middleware fixture produce chunked middleware

--- tests/fixtures/middleware/next.config.js
+++ tests/fixtures/middleware/next.config.js
@@ -4,6 +4,13 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    // this is a trigger to generate multiple `.next/server/middleware-[hash].js` files instead of
+    // single `.next/server/middleware.js` file
+    config.optimization.splitChunks.maxSize = 100_000
+
+    return config
+  },
 }
 
 module.exports = nextConfig
